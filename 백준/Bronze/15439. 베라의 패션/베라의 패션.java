ebbd00c05af688f3d472810307e31cbf685c0b45
권기오 <43674053+kwongio@users.noreply.github.com>
2024-01-16 15:52:11 +0900

    [Bronze IV] Title: 베라의 패션, Time: 124 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        System.out.println(N * (N-1));
+
+
+
+    }
+}
