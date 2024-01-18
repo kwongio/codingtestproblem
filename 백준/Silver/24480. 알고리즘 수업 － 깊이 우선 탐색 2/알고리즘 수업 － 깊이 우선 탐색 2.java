22ee2db1d018b3cbf4ddad0c123c87032b174b51
권기오 <43674053+kwongio@users.noreply.github.com>
2024-01-18 18:27:13 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 1892 ms, Memory: 99836 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.sql.PreparedStatement;
+import java.util.*;
+
+public class Main {
+    static boolean[] visited;
+    static List<List<Integer>> list;
+    static int[] seq;
+    static int count = 1;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int R = Integer.parseInt(st.nextToken());
+        visited = new boolean[N + 1];
+        list = new ArrayList<>();
+        seq = new int[N + 1];
+
+        for (int i = 0; i < N + 1; i++) {
+            list.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            list.get(start).add(end);
+            list.get(end).add(start);
+        }
+        for (int i = 1; i < N + 1; i++) {
+            Collections.sort(list.get(i) ,Collections.reverseOrder());
+        }
+        visited[R] = true;
+        seq[R] = count++;
+        DFS(R);
+        for (int i = 1; i < N + 1 ; i++) {
+            System.out.println(seq[i]);
+        }
+    }
+
+    private static void DFS(int start) {
+        for (Integer v : list.get(start)) {
+            if (!visited[v]) {
+                visited[v] = true;
+                seq[v] = count++;
+                DFS(v);
+            }
+        }
+    }
+}
