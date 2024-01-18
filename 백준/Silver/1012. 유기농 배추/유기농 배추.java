e3517bcc7a916521e7e88fe7ec78edf863e983db
권기오 <43674053+kwongio@users.noreply.github.com>
2024-01-18 20:22:55 +0900

    [Silver II] Title: 유기농 배추, Time: 164 ms, Memory: 16028 KB -BaekjoonHub

---
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int N;
+    static int M;
+    static int T;
+    static int K;
+
+    static int[][] map;
+    static boolean[][] visited;
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
+
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        T = Integer.parseInt(st.nextToken());
+
+
+        for (int t = 0; t < T; t++) {
+            int count = 0;
+            st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken());
+            K = Integer.parseInt(st.nextToken());
+            map = new int[N + 1][M+ 1];
+            visited = new boolean[N + 1][M + 1];
+            for (int i = 0; i < K; i++) {
+                st = new StringTokenizer(br.readLine());
+                int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
+                map[x][y] = 1;
+            }
+
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < M; j++) {
+                    if (map[i][j] == 1 && !visited[i][j]) {
+                        BFS(i, j);
+                        count++;
+                    }
+                }
+            }
+            System.out.println(count);
+        }
+
+    }
+
+    private static void BFS(int x, int y) {
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{x, y});
+        visited[x][y] =true;
+        while (!q.isEmpty()) {
+            int[] now = q.poll();
+            for (int i = 0; i < 4; i++) {
+                int nx = dx[i] + now[0];
+                int ny = dy[i] + now[1];
+                if (nx >= 0 && nx < N && ny >= 0 && ny < M) {
+                    if (!visited[nx][ny] && map[nx][ny] == 1) {
+                        visited[nx][ny] = true;
+                        q.add(new int[]{nx, ny});
+                    }
+                }
+            }
+        }
+    }
+}
