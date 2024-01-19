ee79affb372b885fe19d4f45c941744d781676d0
권기오 <43674053+kwongio@users.noreply.github.com>
2024-01-19 12:42:59 +0900

    [Gold V] Title: 토마토, Time: 620 ms, Memory: 121256 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,75 +1,66 @@
-
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import javax.swing.plaf.IconUIResource;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-    static int n, m;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     static int[][] map;
+    static boolean[][] visited;
     static int[] dx = {1, -1, 0, 0};
     static int[] dy = {0, 0, 1, -1};
 
-    static Queue<Tomato> q;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        m = sc.nextInt();
-        n = sc.nextInt();
-        q = new LinkedList<>();
+    public static void main(String[] args) throws IOException {
 
-        map = new int[n][m];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                int v = sc.nextInt();
-                if (v == 1) {
-                    q.add(new Tomato(i, j));
+        map = new int[N][M];
+        visited = new boolean[N][M];
+        Queue<int[]> q = new LinkedList<>();
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+                if (map[i][j] == 1) {
+                    q.add(new int[]{i, j});
                 }
-                map[i][j] = v;
             }
         }
         while (!q.isEmpty()) {
-            Tomato now = q.poll();
+            int[] now = q.poll();
             for (int i = 0; i < 4; i++) {
-                int nx = dx[i] + now.x;
-                int ny = dy[i] + now.y;
-                if (nx >= 0 && ny >= 0 && nx < n && ny < m) {
+                int nx = dx[i] + now[0];
+                int ny = dy[i] + now[1];
+                if (nx >= 0 && nx < N && ny >= 0 && ny < M) {
                     if (map[nx][ny] == 0) {
-                        q.offer(new Tomato(nx, ny));
-                        map[nx][ny] = map[now.x][now.y] + 1;
+                        map[nx][ny] = map[now[0]][now[1]] + 1;
+                        q.add(new int[]{nx, ny});
                     }
                 }
             }
         }
-        int answer = Integer.MIN_VALUE;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                max = Math.max(max, map[i][j]);
                 if (map[i][j] == 0) {
                     System.out.println(-1);
                     return;
-                } else {
-                    answer = Math.max(answer, map[i][j]);
                 }
             }
         }
-        if (answer == 1) {
+        if (max == 1) {
             System.out.println(0);
+
         } else {
-            System.out.println(answer - 1);
 
+            System.out.println(max - 1);
         }
-    }
-
-}
-
-class Tomato {
-    int x;
-    int y;
-
-    public Tomato(int x, int y) {
-        this.x = x;
-        this.y = y;
     }
 }
