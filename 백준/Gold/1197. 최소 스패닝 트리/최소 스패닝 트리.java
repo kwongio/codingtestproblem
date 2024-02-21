f55d9d3f9d1a56ddad93d0754f60381a4731b063
권기오 <43674053+kwongio@users.noreply.github.com>
2024-02-21 22:58:26 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 668 ms, Memory: 50956 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+class Main {
+
+    static int[] parent;
+    static List<int[]> list = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int V = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
+        parent = new int[V + 1];
+        for (int i = 1; i <= V; i++) {
+            parent[i] = i;
+        }
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            list.add(new int[]{a, b, c});
+        }
+        list.sort((o1, o2) -> o1[2] - o2[2]);
+        int answer = 0;
+        for (int[] arr : list) {
+            if (find(arr[0]) != find(arr[1])) {
+                union(arr[0], arr[1]);
+                answer+= arr[2];
+            }
+        }
+        System.out.println(answer);
+
+    }
+
+    static void union(int a, int b) {
+        a = find(a);
+        b = find(b);
+        if (a != b) {
+            parent[b] = a;
+        }
+    }
+
+    static int find(int a) {
+        if (parent[a] == a) return a;
+        return parent[a] = find(parent[a]);
+    }
+
+}
