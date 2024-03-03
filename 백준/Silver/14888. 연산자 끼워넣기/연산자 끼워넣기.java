b7279a22409705a9e7d320c34e3e7c2a93661f90
권기오 <43674053+kwongio@users.noreply.github.com>
2024-03-03 12:22:22 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 132 ms, Memory: 16092 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -1,56 +1,57 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static StringBuilder sb;
     static int N;
-    static int[] operation;
     static int[] arr;
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static int max = Integer.MIN_VALUE;
-    static int min = Integer.MAX_VALUE;
+    static int[] operation;
+    static long max = Long.MIN_VALUE;
+    static long min = Long.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(br.readLine());
         arr = new int[N];
-        st = new StringTokenizer(br.readLine());
         operation = new int[4];
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < 4; i++) {
             operation[i] = Integer.parseInt(st.nextToken());
         }
+
         DFS(1, arr[0]);
         System.out.println(max);
         System.out.println(min);
-
     }
 
-    private static void DFS(int depth, int sum) {
+    private static void DFS(int depth, long num) {
         if (depth == N) {
-            max = Math.max(sum, max);
-            min = Math.min(sum, min);
+            max = Math.max(max, num);
+            min = Math.min(min, num);
+
             return;
         }
 
         for (int i = 0; i < 4; i++) {
             if (operation[i] > 0) {
                 operation[i]--;
-                if (i == 0) DFS(depth + 1, sum + arr[depth]);
-                if (i == 1) DFS(depth + 1, sum - arr[depth]);
-                if (i == 2) DFS(depth + 1, sum * arr[depth]);
-                if (i == 3) DFS(depth + 1, sum / arr[depth]);
+                if (i == 0) {
+                    DFS(depth + 1, num + arr[depth]);
+                } else if (i == 1) {
+                    DFS(depth + 1, num - arr[depth]);
+                } else if (i == 2) {
+                    DFS(depth + 1, num * arr[depth]);
+                } else if (i == 3) {
+                    DFS(depth + 1, num / arr[depth]);
+                }
                 operation[i]++;
             }
         }
-
-
     }
-
 }
