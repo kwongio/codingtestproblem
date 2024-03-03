0a73b65b9190fb535f758fe02863076e84ee2fe4
권기오 <43674053+kwongio@users.noreply.github.com>
2024-03-03 21:47:55 +0900

    [Gold III] Title: 다리 만들기, Time: 1428 ms, Memory: 296200 KB -BaekjoonHub

--- 백준/Gold/2146. 다리 만들기/다리 만들기.java
+++ 백준/Gold/2146. 다리 만들기/다리 만들기.java
@@ -1,94 +1,93 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
-class Main {
+public class Main {
+
     static int N;
     static int[][] map;
-    static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, 1, 0, -1};
+    static int landNum = 1;
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
     static boolean[][] visited;
-    static int answer = Integer.MAX_VALUE;
-    static int num = 2;
+    static int min = Integer.MAX_VALUE;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
         map = new int[N][N];
-
+        visited = new boolean[N][N];
         for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
-                map[i][j] = sc.nextInt();
+                map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                if (map[i][j] == 1) {
-                    landNum(i, j);
+                if (map[i][j] != 0 && !visited[i][j]) {
+                    BFS(i, j);
                 }
             }
         }
+//        for (int i = 0; i < N; i++) {
+//            for (int j = 0; j < N; j++) {
+//                System.out.print(map[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
 
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                if (map[i][j] >= 2) {
+                if (map[i][j] != 0) {
                     visited = new boolean[N][N];
-                    makeBridge(i, j);
+                    make(i, j);
                 }
             }
         }
-        System.out.println(answer);
-
-
+        System.out.println(min);
     }
 
-    private static void makeBridge(int i, int j) {
-        Queue<Point> q = new LinkedList<>();
-        q.add(new Point(i, j, 0));
-        int currNum = map[i][j];
-        visited[i][j] = true;
+    private static void make(int x, int y) {
+        int currentLand = map[x][y];
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{x, y, 0});
+        visited[x][y] = true;
         while (!q.isEmpty()) {
-            Point now = q.poll();
-            for (int k = 0; k < 4; k++) {
-                int nx = now.x + dx[k];
-                int ny = now.y + dy[k];
-                if (nx >= 0 && ny >= 0 && nx < N && ny < N && map[nx][ny] != currNum && !visited[nx][ny]) {
+            int[] now = q.poll();
+            for (int i = 0; i < 4; i++) {
+                int nx = dx[i] + now[0];
+                int ny = dy[i] + now[1];
+                if (nx >= 0 && nx < N && ny >= 0 && ny < N && currentLand != map[nx][ny] && !visited[nx][ny]) {
                     visited[nx][ny] = true;
                     if (map[nx][ny] == 0) {
-                        q.offer(new Point(nx, ny, now.cnt + 1));
+                        q.add(new int[]{nx, ny, now[2] + 1});
                     } else {
-                        answer = Math.min(answer, now.cnt);
+                        min = Math.min(min, now[2]);
                     }
                 }
             }
         }
     }
 
-    private static void landNum(int i, int j) {
-        Queue<Point> q = new LinkedList<>();
-        q.add(new Point(i, j, 0));
-        map[i][j] = num;
+    private static void BFS(int x, int y) {
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{x, y});
+        map[x][y] = landNum;
+        visited[x][y] = true;
         while (!q.isEmpty()) {
-            Point now = q.poll();
-            for (int k = 0; k < 4; k++) {
-                int nx = now.x + dx[k];
-                int ny = now.y + dy[k];
-                if (nx >= 0 && ny >= 0 && nx < N && ny < N && map[nx][ny] == 1) {
-                    map[nx][ny] = num;
-                    q.add(new Point(nx, ny, 0));
+            int[] now = q.poll();
+            for (int i = 0; i < 4; i++) {
+                int nx = dx[i] + now[0];
+                int ny = dy[i] + now[1];
+                if (nx >= 0 && nx < N && ny >= 0 && ny < N && map[nx][ny] == 1 && !visited[nx][ny]) {
+                    map[nx][ny] = landNum;
+                    visited[nx][ny] = true;
+                    q.add(new int[]{nx, ny});
                 }
             }
         }
-        num++;
-    }
-}
-
-class Point {
-    int x;
-    int y;
-    int cnt;
-
-    public Point(int x, int y, int cnt) {
-        this.x = x;
-        this.y = y;
-        this.cnt = cnt;
+        landNum++;
     }
 }
