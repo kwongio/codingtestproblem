9bcdf2fe868bf00dbd0df4317d9e015b0d299613
권기오 <43674053+kwongio@users.noreply.github.com>
2024-03-05 21:53:14 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 716 ms, Memory: 69872 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
@@ -1,77 +1,69 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static List<List<int[]>> list;
+    static int N, E;
+    static List<int[]>[] list;
     static int[] distance;
     static boolean[] visited;
-    static int N;
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
-        int E = Integer.parseInt(st.nextToken());
-        list = new ArrayList<>();
-        for (int i = 0; i <= N; i++) list.add(new ArrayList<>());
+        E = Integer.parseInt(st.nextToken());
+        list = new ArrayList[N + 1];
 
+        for (int i = 1; i <= N; i++) list[i] = new ArrayList<>();
         for (int i = 0; i < E; i++) {
             st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            int cost = Integer.parseInt(st.nextToken());
-            list.get(start).add(new int[]{end, cost});
-            list.get(end).add(new int[]{start, cost});
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            list[a].add(new int[]{b, c});
+            list[b].add(new int[]{a, c});
         }
         st = new StringTokenizer(br.readLine());
         int v1 = Integer.parseInt(st.nextToken());
         int v2 = Integer.parseInt(st.nextToken());
 
-        int result = dikstra(1, v1);
-        result += dikstra(v1, v2);
-        result += dikstra(v2, N);
+        int result = dijkstra(1, v1);
+        result += dijkstra(v1, v2);
+        result += dijkstra(v2, N);
 
-
-        int result2 = dikstra(1, v2);
-        result2 += dikstra(v2, v1);
-        result2 += dikstra(v1, N);
-        if (result2 >= 100000001 && result >= 100000001) {
+        int result2 = dijkstra(1, v2);
+        result2 += dijkstra(v2, v1);
+        result2 += dijkstra(v1, N);
+        if(result2 >= 100000001 && result >= 100000001){
             System.out.println(-1);
         }else{
-            result = Math.min(result2, result);
+             result = Math.min(result2, result);
             System.out.println(result);
-
         }
-
-
     }
 
-    private static int dikstra(int start, int end) {
-        distance = new int[N + 1];
+    private static int dijkstra(int s, int e) {
         visited = new boolean[N + 1];
+        distance = new int[N + 1];
         for (int i = 1; i < N + 1; i++) {
             distance[i] = 100000001;
         }
-
-        PriorityQueue<int[]> q = new PriorityQueue<>(Comparator.comparingInt(o -> o[1]));
-        q.add(new int[]{start, 0});
-        distance[start] = 0;
+        PriorityQueue<int[]> q = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
+        q.add(new int[]{s, 0});
+        distance[s] = 0;
         while (!q.isEmpty()) {
             int[] now = q.poll();
             if (visited[now[0]]) continue;
             visited[now[0]] = true;
-            for (int[] next : list.get(now[0])) {
+            for (int[] next : list[now[0]]) {
                 if (distance[next[0]] > distance[now[0]] + next[1]) {
                     distance[next[0]] = distance[now[0]] + next[1];
                     q.add(new int[]{next[0], distance[next[0]]});
                 }
             }
         }
-        return distance[end];
+        return distance[e];
     }
-
 }
