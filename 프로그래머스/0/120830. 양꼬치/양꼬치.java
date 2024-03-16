d4fc5fb1427a26943316a7b57b974f9b1a2b4ffb
권기오 <43674053+kwongio@users.noreply.github.com>
2024-03-17 00:57:02 +0900

    [level 0] Title: 양꼬치, Time: 0.03 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.java
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.java
@@ -1,10 +1,5 @@
 class Solution {
     public int solution(int n, int k) {
-        int answer = 0;
-        int service = n / 10;
-        service = Math.max(k - service, 0);
-        answer += service * 2000;
-        answer += 12000*n;
-        return answer;
+        return n * 12000 + k * 2000 - (n / 10 * 2000);
     }
 }
