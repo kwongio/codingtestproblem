67b393a16a494fc7d5e1b2f550c585ef8725a094
권기오 <43674053+kwongio@users.noreply.github.com>
2024-03-22 22:55:25 +0900

    [Silver III] Title: N과 M (2), Time: 152 ms, Memory: 16164 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,37 +1,42 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    static StringBuilder sb;
-    static boolean[] visited;
-    static int N;
-    static int M;
+    static int N, M;
 
-    public static void main(String[] args) throws IOException {
+    static int[] arr;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-
+        arr = new int[M];
         visited = new boolean[N + 1];
-        DFS(0, "", 1);
+        DFS(0 , 1);
 
 
     }
 
-    private static void DFS(int L, String select, int s) {
-        if (L == M) {
-            System.out.println(select);
+    private static void DFS(int depth, int start) {
+        if (M == depth) {
+            for (int i : arr) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
             return;
         }
-
-        for (int i = s; i <= N; i++) {
-            DFS(L + 1, select + i + " ", i + 1);
+        for (int i = start; i <= N; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                arr[depth] = i;
+                DFS(depth + 1 , i + 1);
+                arr[depth] = 0;
+                visited[i] = false;
+            }
         }
 
-
     }
-
 
 }
