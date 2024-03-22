e9d9efb8c65f608d6204794f6deba0f03ff1408e
권기오 <43674053+kwongio@users.noreply.github.com>
2024-03-22 18:38:55 +0900

    [level 2] Title: 피로도, Time: 0.41 ms, Memory: 64.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Solution {
+    int answer = 0;
+    boolean[] visited;
+    public int solution(int k, int[][] dungeons) {
+        visited = new boolean[dungeons.length];
+        DFS(k, dungeons, 0);
+        return answer;
+    }
+    void DFS(int k , int[][] dungeons, int count){
+        if(k < 0) return;
+        answer = Math.max(answer, count);
+        
+        for(int i = 0 ; i < dungeons.length;i++){
+            if(!visited[i]){
+                if(k >= dungeons[i][0]){
+                    visited[i] = true;
+                  DFS(k,dungeons,count);
+                    DFS(k - dungeons[i][1],dungeons,count + 1);   
+                    visited[i] = false;
+                }
+              
+            }
+        }
+    }
+}
