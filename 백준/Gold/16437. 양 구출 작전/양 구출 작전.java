e560a1fe904beb85798db1d6ceaa1822d6b7cf5d
권기오 <43674053+kwongio@users.noreply.github.com>
2024-03-28 22:44:41 +0900

    [Gold III] Title: 양 구출 작전, Time: 588 ms, Memory: 66500 KB -BaekjoonHub

--- 백준/Gold/16437. 양 구출 작전/양 구출 작전.java
+++ 백준/Gold/16437. 양 구출 작전/양 구출 작전.java
@@ -1,52 +1,52 @@
-import javax.naming.spi.DirStateFactory;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
+import java.io.*;
 
 public class Main {
+
     static int N;
     static long[] arr;
     static List<Integer>[] list;
-    static int max = 0;
+    static int[] child;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        arr = new long[N + 1];
-        list = new ArrayList[N + 1];
+        N = Integer.parseInt(br.readLine());
+
+        list = new List[N + 1];
+        child = new int[N + 1];
         for (int i = 1; i <= N; i++) {
             list[i] = new ArrayList<>();
         }
-        for (int i = 2; i <= N; i++) { //i는 현재 섬번호
-            st = new StringTokenizer(br.readLine());
-
-            String t = st.nextToken(); // 양인지 늑대인지 w는 늑대  s양
-            int a = Integer.parseInt(st.nextToken()); // 마리수
-            int p = Integer.parseInt(st.nextToken()); // i번째섬에서 p섬으로 가는곳
+        arr = new long[N + 1];
+        for (int i = 2; i <= N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String t = st.nextToken();
+            int amount = Integer.parseInt(st.nextToken());
+            int next = Integer.parseInt(st.nextToken());
             if (t.equals("W")) {
-                arr[i] = -a;
-            } else if (t.equals("S")) {
-                arr[i] = a;
+                amount *= -1;
             }
-            list[p].add(i);
+            arr[i] = amount;
+            list[i].add(next);
+            child[next]++;
         }
-        DFS(1, -1);
+        Queue<Integer> q = new ArrayDeque<>();
+        for (int i = 2; i <= N; i++) {
+            if (child[i] == 0) {
+                q.add(i); // 자식노드
+            }
+        }
+
+        while (!q.isEmpty()) {
+            int now = q.poll();
+            for (Integer i : list[now]) {
+                if (arr[now] > 0) {
+                    arr[i] += arr[now];
+                }
+                if(--child[i] == 0) q.add(i);
+            }
+
+        }
         System.out.println(arr[1]);
-    }
-
-    private static void DFS(int depth, int pa) {
-
-
-        for (int next : list[depth]) {
-            DFS(next, depth);
-        }
-
-        if (pa != -1) {
-            if (arr[depth] > 0) {
-                arr[pa] += arr[depth];
-            }
-        }
     }
 }
