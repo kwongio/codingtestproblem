504b9d3f68882d99bc8799ab27709ae6a256bc3f
권기오 <43674053+kwongio@users.noreply.github.com>
2024-06-21 13:05:34 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 484 ms, Memory: 63088 KB -BaekjoonHub

--- 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
@@ -1,67 +1,69 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int[] hx = {-1, -2, -2, -1, 1, 2, 2, 1};
-    static int[] hy = {-2, -1, 1, 2, 2, 1, -1, -2};
-    static int[] dx = {1, -1, 0, 0};
-    static int[] dy = {0, 0, 1, -1};
-    static int[][] map;
-    static boolean[][][] visited;
-    static int K;
-    static int H;
-    static int W;
+    static int N;
+    static int M;
+    static int[][] board;
+    static int[] dx = {-2, -1, 1, 2, 2, 1, -1, -2};
+    static int[] dy = {1, 2, 2, 1, -1, -2, -2, -1};
 
+    static int[] dx1 = {1, -1, 0, 0};
+    static int[] dy1 = {0, 0, 1, -1};
+    static int K;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        K = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        K = Integer.parseInt(st.nextToken());
-        st = new StringTokenizer(br.readLine());
-        W = Integer.parseInt(st.nextToken());
-        H = Integer.parseInt(st.nextToken());
-        map = new int[H][W];
-        visited = new boolean[H][W][K + 1];
-        for (int i = 0; i < H; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < W; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        board = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 0; j < M; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        BFS(0, 0);
-        System.out.println(-1);
+
+        System.out.println(BFS(0, 0));
+
     }
 
-    private static void BFS(int x, int y) {
-        Queue<int[]> q = new ArrayDeque<>();
-        q.add(new int[]{x, y, 0, K});
-        visited[0][0][K] = true;
+    private static int BFS(int x, int y) {
+        boolean[][][] visit = new boolean[N][M][K + 1];
+        ArrayDeque<int[]> q = new ArrayDeque<>();
+        q.offer(new int[]{x, y, 0, K});
+        visit[x][y][K] = true;
         while (!q.isEmpty()) {
             int[] now = q.poll();
-            if (now[0] == H - 1 && now[1] == W - 1) {
-                System.out.println(now[2]);
-                System.exit(0);
+            if (now[0] == N - 1 && now[1] == M - 1) {
+                return now[2];
             }
-
-            for (int i = 0; i < 4; i++) {
-                int nx = dx[i] + now[0];
-                int ny = dy[i] + now[1];
-                if (nx >= 0 && nx < H && ny >= 0 && ny < W && map[nx][ny] == 0 && !visited[nx][ny][now[3]]) {
-                    visited[nx][ny][now[3]] = true;
-                    q.add(new int[]{nx, ny, now[2] + 1, now[3]});
+            if (now[3] > 0) {
+                for (int i = 0; i < 8; i++) {
+                    int nx = dx[i] + now[0];
+                    int ny = dy[i] + now[1];
+                    if (nx >= 0 && nx < N && ny >= 0 && ny < M && board[nx][ny] != 1 && !visit[nx][ny][now[3] - 1]) {
+                        visit[nx][ny][now[3] - 1] = true;
+                        q.offer(new int[]{nx, ny, now[2] + 1, now[3] - 1});
+                    }
                 }
             }
-            if (now[3] == 0) continue;
-            for (int i = 0; i < 8; i++) {
-                int nx = hx[i] + now[0];
-                int ny = hy[i] + now[1];
-                if (nx >= 0 && nx < H && ny >= 0 && ny < W && map[nx][ny] == 0 && !visited[nx][ny][now[3] - 1]) {
-                    visited[nx][ny][now[3] - 1] = true;
-                    q.add(new int[]{nx, ny, now[2] + 1, now[3] - 1});
+            for (int i = 0; i < 4; i++) {
+                int nx = dx1[i] + now[0];
+                int ny = dy1[i] + now[1];
+                if (nx >= 0 && nx < N && ny >= 0 && ny < M && board[nx][ny] != 1 && !visit[nx][ny][now[3]]) {
+
+                    visit[nx][ny][now[3]] = true;
+                    q.offer(new int[]{nx, ny, now[2] + 1, now[3]});
                 }
             }
         }
+        return -1;
     }
 }
