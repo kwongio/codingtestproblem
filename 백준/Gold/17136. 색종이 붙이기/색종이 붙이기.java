4c0125eef5d215335e531eb8b01974c9b33083a0
권기오 <43674053+kwongio@users.noreply.github.com>
2024-06-21 14:48:52 +0900

    [Gold II] Title: 색종이 붙이기, Time: 248 ms, Memory: 25180 KB -BaekjoonHub

--- 백준/Gold/17136. 색종이 붙이기/색종이 붙이기.java
+++ 백준/Gold/17136. 색종이 붙이기/색종이 붙이기.java
@@ -1,87 +1,85 @@
-import org.w3c.dom.xpath.XPathResult;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-
+    static int max = Integer.MAX_VALUE;
     static int N = 10;
-    static int[][] map = new int[N][N];
-    static int[] paper = {0, 5, 5, 5, 5, 5};
-    static int answer = Integer.MAX_VALUE;
+    static int[][] paper = new int[10][10];
+    static boolean[][] visit = new boolean[10][10];
+    static int[] colorPaper = new int[]{1, 2, 3, 4, 5};
+    static int[] colorPaperCount = new int[]{5, 5, 5, 5, 5};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        StringTokenizer st;
         for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+            st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
+                paper[i][j] = Integer.parseInt(st.nextToken());
             }
         }
+
         DFS(0, 0, 0);
-
-        if (answer == Integer.MAX_VALUE) {
-            answer = -1;
+        if (max == Integer.MAX_VALUE) {
+            max = -1;
         }
-        System.out.println(answer);
-
-
+        System.out.println(max);
     }
 
     private static void DFS(int x, int y, int count) {
-        if (x >= 9 && y > 9) {
-            answer = Math.min(answer, count);
-            return;
-        }
-        if (answer <= count) {
+        if (x >= 9 && y >= 10) {
+            max = Math.min(max, count);
             return;
         }
 
-        if (y > 9) {
+        if (y >= N) {
             DFS(x + 1, 0, count);
             return;
         }
 
-        if (map[x][y] == 1) {
-            for (int p = 5; p >=1; p--) {
-                if (paper[p] > 0 && check(x, y, p)) {
-                    paper[p]--;
-                    attach(x, y, p, 0);
-                    DFS(x, y + 1, count + 1);
-                    paper[p]++;
-                    attach(x, y, p, 1);
+        if (paper[x][y] == 1 && !visit[x][y]) {
+            for (int i = 4; i >= 0; i--) {
+                if (!visit[x][y] && check(x, y, colorPaper[i]) && colorPaperCount[i] > 0) {
+                    attach(x, y, colorPaper[i]);
+                    DFS(x, y + colorPaper[i], count + 1);
+                    dettach(x, y, colorPaper[i]);
                 }
             }
         } else {
             DFS(x, y + 1, count);
         }
-
     }
 
-    private static void attach(int x, int y, int size, int value) {
+    private static void attach(int x, int y, int size) {
         for (int i = x; i < x + size; i++) {
             for (int j = y; j < y + size; j++) {
-                map[i][j] = value;
+                visit[i][j] = true;
             }
         }
+        colorPaperCount[size - 1]--;
+    }
+
+    private static void dettach(int x, int y, int size) {
+        for (int i = x; i < x + size; i++) {
+            for (int j = y; j < y + size; j++) {
+                visit[i][j] = false;
+            }
+        }
+        colorPaperCount[size - 1]++;
     }
 
 
     private static boolean check(int x, int y, int size) {
+        if (x + size > N || y + size > N) return false;
         for (int i = x; i < x + size; i++) {
             for (int j = y; j < y + size; j++) {
-                if (i < 0 || i >= N || j < 0 || j >= N) {
-                    return false;
-                }
-                if (map[i][j] == 0) {
+                if (paper[i][j] != 1 || visit[i][j]) {
                     return false;
                 }
             }
         }
         return true;
     }
-
 }
