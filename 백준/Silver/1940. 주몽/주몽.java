c9a5615f13f0aa427420e06bea52726053f963ce
권기오 <43674053+kwongio@users.noreply.github.com>
2024-06-30 19:03:07 +0900

    [Silver IV] Title: 주몽, Time: 204 ms, Memory: 16740 KB -BaekjoonHub

---
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -0,0 +1,37 @@
+import javax.swing.plaf.IconUIResource;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        int s = 0;
+        int e = N - 1;
+        int result = 0;
+        while (s < e) {
+            if(arr[s] + arr[e] == M){
+                result++;
+                s++;
+                e--;
+            }else if(arr[s] + arr[e] < M){
+                s++;
+            }else{
+                e--;
+            }
+        }
+        System.out.println(result);
+    }
+}
