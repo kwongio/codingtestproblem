570b647d4ae8cd6cf99bc83c6fec2fa07a7d874a
권기오 <43674053+kwongio@users.noreply.github.com>
2024-06-30 20:18:16 +0900

    [Silver III] Title: 1, Time: 104 ms, Memory: 14064 KB -BaekjoonHub

---
+++ 백준/Silver/4375. 1/1.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input;
+        while ((input = br.readLine()) != null) {
+            int N = Integer.parseInt(input);
+            long count = 1;
+            long num = 1;
+            while (true) {
+                if (num % N == 0) {
+                    System.out.println(count);
+                    break;
+                } else {
+                    num = num * 10 + 1;
+                    num %= N;
+                    count++;
+                }
+            }
+        }
+    }
+}
