a87dbc4d2688273268164a232d200ec3cb7e6885
권기오 <43674053+kwongio@users.noreply.github.com>
2024-06-30 13:50:05 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 104 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,42 +1,25 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
-
-class Main {
-    static int[] arr;
-    static int[] select;
-    static boolean[] visit;
-
+import java.io.*;
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        arr = new int[9];
-        select = new int[7];
-        visit = new boolean[9];
+        int[] arr = new int[9];
+        int sum = 0;
         for (int i = 0; i < 9; i++) {
             arr[i] = Integer.parseInt(br.readLine());
+            sum += arr[i];
         }
-
-        DFS(0, 0);
-    }
-
-    private static void DFS(int depth, int start) {
-        if (depth == 7) {
-            if (Arrays.stream(select).sum() == 100) {
-                Arrays.sort(select);
-                for (int i = 0; i < 7; i++) {
-                    System.out.println(select[i]);
+        for (int i = 0; i < 8; i++) {
+            for (int j = i+1; j < 9; j++) {
+                if (sum - arr[i] - arr[j] == 100) {
+                    arr[i] = 101;
+                    arr[j] = 101;
+                    Arrays.sort(arr);
+                    for (int k = 0; k <7; k++) {
+                        System.out.println(arr[k]);
+                    }
+                    return;
                 }
-                System.exit(0);
-            }
-            return;
-        }
-        for (int i = start; i < 9; i++) {
-            if (!visit[i]) {
-                visit[i] = true;
-                select[depth] = arr[i];
-                DFS(depth + 1, i + 1);
-                visit[i] = false;
             }
         }
     }
