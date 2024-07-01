b55dc5de6414c52e2d2a9ba5d11c3645ebaf95db
권기오 <43674053+kwongio@users.noreply.github.com>
2024-07-01 22:22:02 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 1048 ms, Memory: 44932 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
@@ -1,59 +1,49 @@
-
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
-import java.util.Stack;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
+    static int N, K;
+    static int[] visit = new int[100001];
+    static int[] pre = new int[100001];
 
-    static int N, M;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        BFS(N);
+    }
 
-    static int[] visited = new int[100001];
-    static int[] predecessor = new int[100001];
-
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        M = sc.nextInt();
-        Queue<Integer> q = new LinkedList<>();
-        q.add(N);
-        visited[N] = 1;
+    private static void BFS(int s) {
+        ArrayDeque<Integer> q = new ArrayDeque<>();
+        q.offer(s);
         while (!q.isEmpty()) {
             int now = q.poll();
-            if (now == M) {
-                System.out.println(visited[now] - 1);
-                break;
+            if (now == K) {
+                System.out.println(visit[now]);
+                Stack<Integer> stack = new Stack<>();
+                int i = K;
+                while (i != N) {
+                    int item = pre[i];
+                    stack.push(item);
+                    i = item;
+                }
+                while (!stack.isEmpty()) {
+                    System.out.print(stack.pop()+ " ");
+                }
+                System.out.println(K);
+                System.exit(0);
             }
-
-            if (now + 1 <= 100000 && visited[now + 1] == 0) {
-                visited[now + 1] = visited[now] + 1;
-                predecessor[now + 1] = now;
-                q.add(now + 1);
+            for (int n : new int[]{now + 1, now * 2, now - 1}) {
+                if (0 <= n && n <= 100000) {
+                    if (visit[n] == 0) {
+                        visit[n] = visit[now] + 1;
+                        q.offer(n);
+                        pre[n] = now;
+                    }
+                }
             }
-            if (now - 1 >= 0 && visited[now - 1] == 0) {
-                visited[now - 1] = visited[now] + 1;
-                predecessor[now - 1] = now;
-                q.add(now - 1);
-            }
-
-
-            if (now * 2 <= 100000 && visited[now * 2] == 0) {
-                visited[now * 2] = visited[now] + 1;
-                predecessor[now * 2] = now;
-                q.add(now * 2);
-            }
-        }
-        int index = M;
-        Stack<Integer> stack = new Stack<>();
-        stack.push(M);
-        while (index != N) {
-            stack.push(predecessor[index]);
-            index = predecessor[index];
-        }
-
-        while (!stack.isEmpty()) {
-            System.out.println(stack.pop());
         }
     }
 }
