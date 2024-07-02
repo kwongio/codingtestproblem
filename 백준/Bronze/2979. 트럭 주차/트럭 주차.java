b42e36ee0916b48f555bb2e33052eb4931b2e118
권기오 <43674053+kwongio@users.noreply.github.com>
2024-07-02 11:33:42 +0900

    [Bronze II] Title: 트럭 주차, Time: 100 ms, Memory: 14008 KB -BaekjoonHub

--- 백준/Bronze/2979. 트럭 주차/트럭 주차.java
+++ 백준/Bronze/2979. 트럭 주차/트럭 주차.java
@@ -1,38 +1,48 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.*;
-import java.io.*;
-
 
 public class Main {
+    static int A, B, C;
+
     static int[] arr;
+    static int[] cost;
+    static int start = Integer.MAX_VALUE;
+    static int end = Integer.MIN_VALUE;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int[] cost = new int[3 + 1];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= 3; i++) {
-            cost[i] = Integer.parseInt(st.nextToken());
-        }
         arr = new int[101];
-        int start = Integer.MAX_VALUE;
-        int end = Integer.MIN_VALUE;
+        cost = new int[3];
+        A = Integer.parseInt(st.nextToken());
+        B = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+
         for (int i = 0; i < 3; i++) {
             st = new StringTokenizer(br.readLine());
-            int s = Integer.parseInt(st.nextToken());
-            int e = Integer.parseInt(st.nextToken());
-            start = Math.min(start, s);
-            end = Math.max(end, e);
-            for (int j = s; j < e; j++) {
-                arr[j]++;
-            }
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            start = Math.min(start, a);
+            end = Math.max(end, b);
+            arr[a]++;
+            arr[b]--;
         }
-//        System.out.println(Arrays.toString(arr));
-        int result = 0;
+        int now = 0;
         for (int i = start; i <= end; i++) {
-            if (arr[i] > 0) {
-                result += cost[arr[i]] * arr[i];
+            now += arr[i];
+            arr[i] = now;
+        }
+        int answer = 0;
+        for (int i = start; i <= end; i++) {
+            if(arr[i] == 1){
+                answer += A * arr[i];
+            }else if(arr[i] == 2){
+                answer += B * arr[i];
+            }else if(arr[i] == 3){
+                answer += C * arr[i];
             }
         }
-        System.out.println(result);
+        System.out.println(answer);
     }
 }
