b5473044d266188c5b3f63537081cbfa680af541
권기오 <43674053+kwongio@users.noreply.github.com>
2024-07-04 20:50:26 +0900

    [Gold I] Title: 멀티탭 스케줄링, Time: 132 ms, Memory: 14136 KB -BaekjoonHub

---
+++ 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    static int[] arr;
+    static int N;
+    static int K;
+    static boolean[] visit;
+    static int cnt;
+
+    static Queue<Integer> q = new LinkedList<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        arr = new int[K];
+        visit = new boolean[104];
+        cnt = 0;
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < K; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < K; i++) {
+            int input = arr[i];
+            if (!visit[input]) {
+                if (q.size() == N) {
+                    int removeIdx = -1;
+                    int value = -1;
+                    for (int v : q) {
+                        int nextUse = Integer.MAX_VALUE;
+                        for (int j = i + 1; j < K; j++) {
+                            if (v == arr[j]) {
+                                nextUse = j;
+                                break;
+                            }
+                        }
+                        if (nextUse > removeIdx) {
+                            removeIdx = nextUse;
+                            value = v;
+                        }
+                    }
+                    visit[value] = false;
+                    cnt++;
+                    q.remove(value);
+                }
+                visit[input] = true;
+                q.add(input);
+            }
+        }
+        System.out.println(cnt);
+    }
+}
