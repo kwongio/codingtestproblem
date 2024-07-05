35c70284918661b1ee7f2b2e72f1bed18c084d63
권기오 <43674053+kwongio@users.noreply.github.com>
2024-07-05 21:21:23 +0900

    [Gold IV] Title: 뱀, Time: 140 ms, Memory: 16268 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.java
+++ 백준/Gold/3190. 뱀/뱀.java
@@ -4,82 +4,94 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-
-    static int[][] map;
-    static int length;
-    static int N;
-    static int K;
-    static int L;
-
-    //북 동 남 서
+    static int N, K, L;
+    static int[][] board;
+    static int sx = 1;
+    static int sy = 1;
     static int[] dx = {-1, 0, 1, 0};
     static int[] dy = {0, 1, 0, -1};
     static int d = 1;
-    static int x = 0;
-    static int y = 0;
-    static int answer = 0;
-    static HashMap<Integer, String> hash = new HashMap<>();
-    static List<int[]> snake = new ArrayList<>();
+    static int time;
+    static ArrayDeque<int[]> q = new ArrayDeque<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        map = new int[N][N];
-        st = new StringTokenizer(br.readLine());
-        K = Integer.parseInt(st.nextToken());
-        snake.add(new int[]{0, 0});
+        N = Integer.parseInt(br.readLine());
+        K = Integer.parseInt(br.readLine());
+        board = new int[N + 1][N + 1];
+        StringTokenizer st;
         for (int i = 0; i < K; i++) {
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken()) - 1;
-            int b = Integer.parseInt(st.nextToken()) - 1;
-            map[a][b] = 1;
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            board[a][b] = 1;
         }
-        st = new StringTokenizer(br.readLine());
-        L = Integer.parseInt(st.nextToken());
-
+        q.add(new int[]{sx, sy});
+        board[sx][sy] = 2;
+        L = Integer.parseInt(br.readLine());
         for (int i = 0; i < L; i++) {
             st = new StringTokenizer(br.readLine());
-            int X = Integer.parseInt(st.nextToken());
-            String C = st.nextToken();
-            hash.put(X, C);
-
+            int X = Integer.parseInt(st.nextToken());  //X초 후 C로 방향전환
+            char C = st.nextToken().charAt(0);   // L일때 왼쪽  D일때 오른쪽
+            for (int j = time; j < X; j++) {
+                time++;
+                int nx = sx + dx[d];
+                int ny = sy + dy[d];
+                if (nx >= 1 && nx <= N && ny >= 1 && ny <= N) {
+                    if (board[nx][ny] == 1) {
+                        q.add(new int[]{nx, ny});
+                        board[nx][ny] = 2;
+                    } else if (board[nx][ny] == 2) {
+                        System.out.println(time);
+                        System.exit(0);
+                    } else if (board[nx][ny] == 0) {
+                        if (!q.isEmpty()) {
+                            int[] now = q.poll();
+                            board[now[0]][now[1]] = 0;
+                        }
+                        q.add(new int[]{nx, ny});
+                        board[nx][ny] = 2;
+                    }
+                    sx = nx;
+                    sy = ny;
+                } else {
+                    System.out.println(time);
+                    System.exit(0);
+                }
+            }
+            if (C == 'L') {
+                d = (d - 1 + 4) % 4;
+            } else if (C == 'D') {
+                d = (d + 1 + 4) % 4;
+            }
         }
         while (true) {
-            answer++;
-            int nx = dx[d] + x;
-            int ny = dy[d] + y;
-            if (nx >= 0 && nx < N && ny >= 0 && ny < N) {
-                for (int[] s : snake) {
-                    if (s[0] == nx && s[1] == ny) {
-                        System.out.println(answer);
-                        return;
+            time++;
+            int nx = sx + dx[d];
+            int ny = sy + dy[d];
+            if (nx >= 1 && nx <= N && ny >= 1 && ny <= N) {
+                if (board[nx][ny] == 1) {
+                    q.add(new int[]{nx, ny});
+                    board[nx][ny] = 2;
+                } else if (board[nx][ny] == 2) {
+                    System.out.println(time);
+                    System.exit(0);
+                } else if (board[nx][ny] == 0) {
+                    if (!q.isEmpty()) {
+                        int[] now = q.poll();
+                        board[now[0]][now[1]] = 0;
                     }
+                    q.add(new int[]{nx, ny});
+                    board[nx][ny] = 2;
                 }
-
-                if (map[nx][ny] == 1) {
-                    map[nx][ny] = 0;
-                    snake.add(new int[]{nx, ny});
-                } else {
-                    snake.add(new int[]{nx, ny});
-                    snake.remove(0);
-
-                }
-                x = nx;
-                y = ny;
-                if (hash.containsKey(answer)) {
-                    String s = hash.get(answer);
-                    if (s.equals("L")) {
-                        d = (d - 1 + 4) % 4;
-                    } else {
-                        d = (d + 1 + 4) % 4;
-                    }
-
-                }
+                sx = nx;
+                sy = ny;
             } else {
-                System.out.println(answer);
-                return;
+                System.out.println(time);
+                System.exit(0);
             }
         }
     }
+
 }
+
