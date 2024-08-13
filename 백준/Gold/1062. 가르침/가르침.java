e1c3d31e105269fe35766dc9ba0573f1ec1eb3be
권기오 <43674053+kwongio@users.noreply.github.com>
2024-08-13 20:28:36 +0900

    [Gold IV] Title: 가르침, Time: 304 ms, Memory: 15912 KB -BaekjoonHub

--- 백준/Gold/1062. 가르침/가르침.java
+++ 백준/Gold/1062. 가르침/가르침.java
@@ -1,70 +1,69 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
 
-	static int n, k;
-	static int max = Integer.MIN_VALUE;
-	static boolean[] visited;
-	static String[] word;
+	static int N;
+	static int K;
+	static int max = 0;
+	static String[] words;
+	static boolean[] visit = new boolean[26];
 
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		words = new String[N];
+		for (int i = 0; i < N; i++) {
+			String str = br.readLine();
+			str = str.replace("a", "");
+			str = str.replace("t", "");
+			str = str.replace("n", "");
+			str = str.replace("i", "");
+			str = str.replace("c", "");
+			words[i] = str;
 
-		n = scan.nextInt();
-		k = scan.nextInt();
-
-		scan.nextLine();
-		word = new String[n];
-		for (int i = 0; i < n; i++) {
-			String str = scan.nextLine();
-			str = str.replace("anta", "");
-			str = str.replace("tica", "");
-			word[i] = str;
 		}
-
-		if (k < 5) {
-			System.out.println("0");
-			return;
-		} else if (k == 26) {
-			System.out.println(n);
-			return;
+		visit['a' - 'a'] = true;
+		visit['n' - 'a'] = true;
+		visit['t' - 'a'] = true;
+		visit['i' - 'a'] = true;
+		visit['c' - 'a'] = true;
+		if (K < 5) {
+			System.out.println(max);
+		} else {
+			DFS(0, 0);
+			System.out.println(max);
 		}
-
-		visited = new boolean[26];
-		visited['a' - 'a'] = true;
-		visited['c' - 'a'] = true;
-		visited['i' - 'a'] = true;
-		visited['n' - 'a'] = true;
-		visited['t' - 'a'] = true;
-
-		backtracking(0, 0);
-		System.out.println(max);
 	}
 
-	public static void backtracking(int alpha, int len) {
-		if (len == k - 5) {
+	static void DFS(int depth, int s) {
+		if (depth == K - 5) {
 			int count = 0;
-			for (int i = 0; i < n; i++) { // 뽑은 알파벳으로 몇개의 단어를 읽을 수 있는지 카운트.
-				boolean read = true;
-				for (int j = 0; j < word[i].length(); j++) {
-					if (!visited[word[i].charAt(j) - 'a']) {
-						read = false;
+			for (int i = 0; i < words.length; i++) {
+				boolean flag = true;
+				for (int j = 0; j < words[i].length(); j++) {
+					if (!visit[words[i].charAt(j) - 'a']) {
+						flag = false;
 						break;
 					}
 				}
-				if (read)
+				if (flag) {
 					count++;
+				}
 			}
 			max = Math.max(max, count);
 			return;
 		}
 
-		for (int i = alpha; i < 26; i++) {
-			if (visited[i] == false) {
-				visited[i] = true;
-				backtracking(i, len + 1);
-				visited[i] = false;
+		for (int i = s; i < 26; i++) {
+			if (visit[i]) {
+				continue;
 			}
+			visit[i] = true;
+			DFS(depth + 1, i + 1);
+			visit[i] = false;
 		}
 	}
 }
