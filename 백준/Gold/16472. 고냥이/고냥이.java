3476852b09ed9471899882efe277eee6e2a98c9c
권기오 <43674053+kwongio@users.noreply.github.com>
2024-08-20 18:50:27 +0900

    [Gold IV] Title: 고냥이, Time: 128 ms, Memory: 15680 KB -BaekjoonHub

--- 백준/Gold/16472. 고냥이/고냥이.java
+++ 백준/Gold/16472. 고냥이/고냥이.java
@@ -17,22 +17,23 @@ public class Main {
 		visit = new int[26];
 		arr = br.readLine().toCharArray();
 		int ans = 0;
+		int kind = 0;
 		for (int R = 0, L = 0; R < arr.length; R++) {
 			// R 번째를 오른쪽에 추가
+			if (visit[arr[R] - 'a'] == 0) {
+				kind++;
+			}
 			visit[arr[R] - 'a']++;
 			// 불가능하면 가능할때까지 L을 이동
 
-			while (true) {
-				int kind = 0;
-				for (int i = 0; i < 26; i++) {
-					if (visit[i] != 0)
-						kind++;
-				}
-				if (kind <= N)
-					break;
+			while (kind > N) {
 				visit[arr[L] - 'a']--;
+				if (visit[arr[L] - 'a'] == 0) {
+					kind--;
+				}
 				L++;
 			}
+
 			// 정답 개신
 			ans = Math.max(ans, R - L + 1);
 		}
