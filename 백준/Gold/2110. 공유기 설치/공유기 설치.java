24f22c6b3fc8b408116d39179eee7e3783f346e7
권기오 <43674053+kwongio@users.noreply.github.com>
2024-08-20 15:10:19 +0900

    [Gold IV] Title: 공유기 설치, Time: 280 ms, Memory: 30348 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -1,50 +1,53 @@
+import java.util.*;
+
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
 import java.io.*;
-import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static int N;
+	static int C;
+	static int[] arr;
 
-static int[] house;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int C = Integer.parseInt(st.nextToken());
-        house = new int[N];
-        for (int i = 0; i < N; i++) {
-            house[i] = Integer.parseInt(br.readLine());
-        }
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+		arr = new int[N];
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		Arrays.sort(arr);
 
-        Arrays.sort(house);
-        int left = 0;
-        int right = house[N - 1] - house[0] + 1;
+		int s = 1;
+		int e = 1000000000;
+		int ret = 1;
+		while (s <= e) {
+			int mid = s + (e - s) / 2;
+			if (check(mid)) {
+				s = mid + 1;
+				ret = Math.max(ret, mid);
+			} else {
+				e = mid - 1;
+			}
+		}
+		System.out.println(ret);
+	}
 
-        while (left < right) {
-            int mid = (left + right) / 2;
+	static boolean check(int v) {
+		int s = arr[0];
+		int count = 1;
+		for (int i = 1; i < arr.length; i++) {
+			if (arr[i] - s >= v) {
+				count++;
+				s = arr[i];
+				if (count == C) {
+					return true;
+				}
+			}
+		}
 
-            if (install(mid) < C) {
-                right = mid;
-            }else{
-                left = mid + 1;
-            }
-        }
-        System.out.println(left - 1);
-    }
-
-    private static int install(int distance) {
-
-        int count = 1;
-        int lastInstall = house[0];
-
-        for (int i = 1; i < house.length; i++) {
-            if (house[i] - lastInstall >= distance) {
-                count++;
-                lastInstall = house[i];
-            }
-        }
-        return count;
-    }
-
+		return false;
+	}
 }
