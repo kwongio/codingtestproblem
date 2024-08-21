2913e4890d5e920a74623aadc529b0b5905b7e7f
권기오 <43674053+kwongio@users.noreply.github.com>
2024-08-21 20:37:01 +0900

    [Silver II] Title: 랜선 자르기, Time: 160 ms, Memory: 17128 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -1,41 +1,38 @@
+import java.util.*;
 import java.io.*;
-import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Main {
-    static int[] arr;
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static int K;
+	static int N;
+	static int[] a;
 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		K = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
+		a = new int[K];
+		for (int i = 0; i < K; i++) {
+			a[i] = Integer.parseInt(br.readLine());
+		}
+		long s = 1;
+		long e = Arrays.stream(a).max().getAsInt();
+		while (s <= e) {
+			long mid = s + (e - s) / 2;
+			if (check(mid)) {
+				s = mid + 1;
+			} else {
+				e = mid - 1;
+			}
+		}
+		System.out.println(s - 1);
+	}
 
-    public static void main(String[] args) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int K = Integer.parseInt(st.nextToken());
-        int N = Integer.parseInt(st.nextToken());
-        arr = new int[K];
-        for (int i = 0; i < K; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-        }
-        long max = Arrays.stream(arr).max().getAsInt();
-        max++;
-        long min = 0;
-        long mid = 0;
-        while (min < max) {
-            long count = 0;
-             mid = (min + max) / 2;
-
-            for (int i = 0; i < arr.length; i++) {
-                count += (arr[i] / mid);
-            }
-
-            if (count < N) {
-                max = mid;
-            } else {
-                min = mid + 1;
-            }
-        }
-        System.out.println(min - 1);
-    }
+	static boolean check(long mid) {
+		int sum = 0;
+		for (int i = 0; i < a.length; i++) {
+			sum += a[i] / mid;
+		}
+		return sum >= N;
+	}
 }
