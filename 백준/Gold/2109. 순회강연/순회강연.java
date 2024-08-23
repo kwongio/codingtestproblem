209e0280746040b0eaff2b78341e2ec2e68852b2
권기오 <43674053+kwongio@users.noreply.github.com>
2024-08-23 20:56:56 +0900

    [Gold III] Title: 순회강연, Time: 240 ms, Memory: 20824 KB -BaekjoonHub

--- 백준/Gold/2109. 순회강연/순회강연.java
+++ 백준/Gold/2109. 순회강연/순회강연.java
@@ -1,41 +1,36 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
 
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
+import java.io.*;
+import java.lang.reflect.Array;
+
 public class Main {
-    static int N;
+	static int N;
+	static List<int[]> list = new ArrayList<int[]>();
 
-    static int[][] arr;
-
-    static PriorityQueue<Integer> q = new PriorityQueue<>((o1, o2) -> o1 - o2);
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        arr = new int[N][2];
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            arr[i][0] = a;
-            arr[i][1] = b;
-        }
-        Arrays.sort(arr, (o1, o2) -> o1[1] - o2[1]);
-
-        for (int i = 0; i < N; i++) {
-            int a = arr[i][0];
-            int b = arr[i][1];
-            q.offer(a);
-            while (q.size() > b) {
-                q.poll();
-            }
-        }
-        int answer = 0;
-        while (!q.isEmpty()) {
-            answer += q.poll();
-        }
-        System.out.println(answer);
-    }
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			list.add(new int[] { a, b });
+		}
+		Collections.sort(list,
+				(o1, o2) -> o1[1] == o2[1] ? Integer.compare(o2[0], o1[0]) : Integer.compare(o1[1], o2[1]));
+		PriorityQueue<Integer> q = new PriorityQueue<Integer>((o1, o2) -> o1 - o2);
+		for (int[] v : list) {
+			q.add(v[0]);
+			while (q.size() > v[1]) {
+				q.poll();
+			}
+		}
+		int ans = 0;
+		for (int v : q) {
+			ans += v;
+		}
+		System.out.println(ans);
+	}
 }
