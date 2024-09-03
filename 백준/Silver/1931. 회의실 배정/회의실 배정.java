e0dec9614855c7fd4a7907d25207ba78b3f7f1dc
권기오 <43674053+kwongio@users.noreply.github.com>
2024-09-03 15:20:15 +0900

    [Silver I] Title: 회의실 배정, Time: 456 ms, Memory: 42112 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.java
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -1,50 +1,30 @@
 import java.io.*;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
+
 
 public class Main {
-    static StringBuilder sb;
-    static int N;
-    static int K;
+	static PriorityQueue<int[]> q = new PriorityQueue<int[]>(
+			(o1, o2) -> o1[1] == o2[1] ? Integer.compare(o1[0], o2[0]) : Integer.compare(o1[1], o2[1]));
+	static int N;
 
-
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-    public static void main(String[] args) throws IOException {
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        int[][] arr = new int[N][2];
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            arr[i][0] = start;
-            arr[i][1] = end;
-        }
-
-        Arrays.sort(arr, (o1, o2) -> {
-            if(o1[1] == o2[1]){
-                return o1[0] - o2[0];
-            }else{
-                return o1[1] - o2[1];
-            }
-        });
-        int count = 1;
-
-        int start = arr[0][0];
-        int end = arr[0][1];
-        for (int i = 1; i < N; i++) {
-            if (arr[i][0] >= end) {
-                start = arr[i][0];
-                end = arr[i][1];
-                count++;
-            }
-
-        }
-        System.out.println(count);
-    }
-
-
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int s = Integer.parseInt(st.nextToken());
+			int e = Integer.parseInt(st.nextToken());
+			q.add(new int[] { s, e });
+		}
+		int ret = 0;
+		int e = -1;
+		while (!q.isEmpty()) {
+			int[] v = q.poll();
+			if (e <= v[0]) {
+				e = v[1];
+				ret++;
+			}
+		}
+		System.out.println(ret);
+	}
 }
