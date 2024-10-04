7244700a6f796dcf5305716e3f05207045fb88a1
권기오 <43674053+kwongio@users.noreply.github.com>
2024-10-04 18:06:50 +0900

    [Gold III] Title: 세 용액, Time: 196 ms, Memory: 15928 KB -BaekjoonHub

--- 백준/Gold/2473. 세 용액/세 용액.java
+++ 백준/Gold/2473. 세 용액/세 용액.java
@@ -1,51 +1,54 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-import javax.swing.plaf.synth.SynthOptionPaneUI;
+class Main {
+    static long[] arr;
 
-import java.io.*;
-import java.lang.reflect.Array;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        arr = new long[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Long.parseLong(st.nextToken());
+        }
+        Arrays.sort(arr);
+        long min = Long.MAX_VALUE;
+        long[] num = new long[3];
+        for (int i = 0; i < N; i++) {
+            int s = i + 1;
+            int e = N - 1;
+            while(s < e){
+//                if(s == i){
+//                    s++;
+//                    continue;
+//                }
+//
+//                if(e == i){
+//                    e--;
+//                    continue;
+//                }
 
-public class Main {
-	static int N;
-	static int[] a;
-
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		a = new int[N];
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) {
-			a[i] = Integer.parseInt(st.nextToken());
-		}
-		Arrays.sort(a);
-		List<Integer> list = new ArrayList<Integer>();
-		long ans = Long.MAX_VALUE;
-		for (int i = 0; i < N; i++) {
-			int s = i + 1;
-			int e = N - 1;
-			while (s < e) {
-				long v = (long)a[s] +(long) a[e] +(long) a[i];
-				long sum = Math.abs((long)a[s] + (long)a[e] + (long)a[i]);
-
-				if (sum < ans) {
-					ans = sum;
-					list.clear();
-					list.add(a[s]);
-					list.add(a[e]);
-					list.add(a[i]);
-				}
-				if (v > 0) {
-					e--;
-				} else {
-					s++;
-				}
-			}
-		}
-
-		Collections.sort(list);
-		for (int v : list) {
-			System.out.print(v + " ");
-		}
-	}
+                long v = arr[i] + arr[s] + arr[e];
+                long ret = Math.abs(arr[i] + arr[s] + arr[e]);
+                if(ret < min){
+                    min = ret;
+                    num[0] = arr[i];
+                    num[1] = arr[s];
+                    num[2] = arr[e];
+                }
+                if(v > 0){
+                    e--;
+                }else {
+                    s++;
+                }
+            }
+        }
+        Arrays.sort(num);
+        for (long i : num) {
+            System.out.print(i + " ");
+        }
+    }
 }
