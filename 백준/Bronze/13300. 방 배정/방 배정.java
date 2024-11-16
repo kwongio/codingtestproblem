6d590d16475132b93570dab96faad97969692426
권기오 <43674053+kwongio@users.noreply.github.com>
2024-11-16 18:56:07 +0900

    [Bronze II] Title: 방 배정, Time: 96 ms, Memory: 14052 KB, Score: 2 point -BaekjoonHub

---
+++ 백준/Bronze/13300. 방 배정/방 배정.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.util.stream.Collectors;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken()); // 참가자
+		int K = Integer.parseInt(st.nextToken()); // 한 방 최대 인원수
+
+		int[][] arr = new int[2][7]; // 성별 1,0 : 학년 1~6;
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			int S = Integer.parseInt(st.nextToken());
+			int Y = Integer.parseInt(st.nextToken());
+			arr[S][Y]++;
+		}
+		int ans = 0;
+		for (int i = 0; i < 2; i++) {
+			for (int j = 1; j <= 6; j++) {
+				ans += (arr[i][j] + 1) / 2;
+			}
+		}
+		System.out.println(ans);
+
+	}
+}
