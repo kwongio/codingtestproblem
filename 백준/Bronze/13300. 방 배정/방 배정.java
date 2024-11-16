5ed6b8956282a0e676b4f0f5d5a21c8dd62183a2
권기오 <43674053+kwongio@users.noreply.github.com>
2024-11-16 19:02:11 +0900

    [Bronze II] Title: 방 배정, Time: 120 ms, Memory: 14576 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/13300. 방 배정/방 배정.java
+++ 백준/Bronze/13300. 방 배정/방 배정.java
@@ -10,6 +10,8 @@ public class Main {
 		int N = Integer.parseInt(st.nextToken()); // 참가자
 		int K = Integer.parseInt(st.nextToken()); // 한 방 최대 인원수
 
+		// 남학생끼리, 여학생끼리, 같은 학년 끼리
+
 		int[][] arr = new int[2][7]; // 성별 1,0 : 학년 1~6;
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
@@ -17,10 +19,11 @@ public class Main {
 			int Y = Integer.parseInt(st.nextToken());
 			arr[S][Y]++;
 		}
+
 		int ans = 0;
 		for (int i = 0; i < 2; i++) {
 			for (int j = 1; j <= 6; j++) {
-				ans += (arr[i][j] + 1) / 2;
+				ans += (arr[i][j] + K - 1) / K;
 			}
 		}
 		System.out.println(ans);
