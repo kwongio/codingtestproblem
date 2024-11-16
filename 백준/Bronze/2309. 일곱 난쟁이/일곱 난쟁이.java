1a0796a31c98ed5d999b30e6437ba4e732ae5070
권기오 <43674053+kwongio@users.noreply.github.com>
2024-11-16 20:54:43 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 104 ms, Memory: 14140 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,26 +1,30 @@
 import java.util.*;
 import java.io.*;
+
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] arr = new int[9];
-        int sum = 0;
-        for (int i = 0; i < 9; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-            sum += arr[i];
-        }
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < i; j++) {
-                if (sum - arr[i] - arr[j] == 100) {
-                    arr[i] = 101;
-                    arr[j] = 101;
-                    Arrays.sort(arr);
-                    for (int k = 0; k <7; k++) {
-                        System.out.println(arr[k]);
-                    }
-                    return;
-                }
-            }
-        }
-    }
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int sum = 0;
+		int[] arr = new int[9];
+		for (int i = 0; i < 9; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+			sum += arr[i];
+		}
+
+		Loop: for (int i = 0; i < 8; i++) {
+			for (int j = i + 1; j < 9; j++) {
+				if (sum - arr[i] - arr[j] == 100) {
+					arr[i] = 101;
+					arr[j] = 101;
+					Arrays.sort(arr);
+					break Loop;
+				}
+			}
+		}
+		for (int i = 0; i < 7; i++) {
+			System.out.println(arr[i]);
+
+		}
+	}
 }
