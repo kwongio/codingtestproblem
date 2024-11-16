8e9979de74b37168fcae14573294cb3d704c448d
권기오 <43674053+kwongio@users.noreply.github.com>
2024-11-16 18:32:10 +0900

    [Silver I] Title: 회전 초밥, Time: 160 ms, Memory: 18288 KB -BaekjoonHub

---
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.java
@@ -0,0 +1,66 @@
+import java.util.*;
+import java.util.stream.Collectors;
+import java.io.*;
+
+public class Main {
+
+	static int N, D, K, C;
+	static int[] arr;
+	static int[] eat;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken()); // 스시 개수
+		int D = Integer.parseInt(st.nextToken()); // 가짓수
+		int K = Integer.parseInt(st.nextToken()); // 연속으로 먹을 스시 개수
+		int C = Integer.parseInt(st.nextToken()); // 쿠폰
+
+		eat = new int[D + 1];
+		arr = new int[N];
+		for (int i = 0; i < N; i++) {
+			int v = Integer.parseInt(br.readLine());
+			arr[i] = v;
+		}
+
+		int count = 0;
+		for (int i = 0; i < K; i++) {
+			if (eat[arr[i]] == 0) {
+				count++;
+			}
+			
+			eat[arr[i]]++;
+		}
+
+		int max = count;;
+		for (int i = 1; i <= N; i++) {
+			if(max <= count) {
+				if(eat[C] == 0) {
+					max = count + 1;
+				}else {
+					max = count;
+				}
+			}
+			
+			
+			int end = (i + K - 1) % N;
+//			System.out.println(end);
+			if(eat[arr[end]] == 0) {
+				count++;
+			}
+			eat[arr[end]]++;
+			
+			
+			eat[arr[i - 1]]--;
+			if(eat[arr[i - 1]] == 0) {
+				count--;
+			}
+		}
+//		if(eat[C] == 0) {
+//			max = count + 1;
+//		}
+		System.out.println(max);
+
+	}
+}
