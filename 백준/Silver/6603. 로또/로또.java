ad0735b3ea63024abd4717df6fb1bba6f70b34ad
권기오 <43674053+kwongio@users.noreply.github.com>
2024-11-22 13:31:57 +0900

    [Silver II] Title: 로또, Time: 152 ms, Memory: 15528 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.java
+++ 백준/Silver/6603. 로또/로또.java
@@ -1,60 +1,46 @@
-import javax.sound.sampled.EnumControl;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 public class Main {
-    static int maxLen = Integer.MIN_VALUE;
-    static boolean[] visited;
-    static int[] select;
     static int N;
-    static int[] numbers;
-
-
+    static List<Integer> arr;
+    static int[] select;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         while (true) {
-            String[] str = br.readLine().split(" ");
-            if (!str[0].equals("0")) {
-                N = Integer.parseInt(str[0]);
-
-                numbers = new int[N];
-                select = new int[6];
-                visited = new boolean[N];
-                for (int i = 0; i < N; i++) {
-                    numbers[i] = Integer.parseInt(str[i+1]);
-                }
-
-                Arrays.sort(numbers);
-                DFS(0, 0);
-                System.out.println();
-            } else {
+            String[] number = br.readLine().split(" ");
+            N = Integer.parseInt(number[0]);
+            if (N == 0) {
                 break;
             }
+            arr = new ArrayList<>();
+            select = new int[6];
+            for (int i = 1; i <= N; i++) {
+                int value = Integer.parseInt(number[i]);
+                arr.add(value);
+            }
+            arr.sort(Comparator.comparing(o -> o));
+            DFS(0, 0);
+            System.out.println();
         }
-
-
     }
 
-    private static void DFS(int depth, int start) {
+    private static void DFS(int depth, int s) {
         if (depth == 6) {
             StringBuilder sb = new StringBuilder();
             for (int i = 0; i < 6; i++) {
-                sb.append(select[i] + " ");
+                sb.append(select[i]).append(" ");
             }
             System.out.println(sb);
             return;
         }
-
-
-        for (int i = start; i < N; i++) {
-
-                select[depth] = numbers[i];
-                DFS(depth + 1, i + 1);
-            }
-
+        for (int i = s; i < N; i++) {
+            select[depth] = arr.get(i);
+            DFS(depth + 1, i + 1);
+        }
     }
-
 }
