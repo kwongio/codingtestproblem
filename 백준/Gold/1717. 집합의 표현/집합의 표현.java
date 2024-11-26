b3b683087ea3a599e7d5eddad15ed004f72925c2
권기오 <43674053+kwongio@users.noreply.github.com>
2024-11-26 11:48:25 +0900

    [Gold V] Title: 집합의 표현, Time: 540 ms, Memory: 51464 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -1,46 +1,52 @@
+import static java.lang.System.in;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.StringTokenizer;
 
-class Main {
-    static int N;
-    static int M;
+public class Main {
 
-    static List<Integer>[] list;
+    public static final String YES = "YES";
+    public static final String NO = "NO";
+    static int N, M;
     static int[] parent;
 
-
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        list = new ArrayList[N + 1];
-        for (int i = 0; i < N + 1; i++) {
-            list[i] = new ArrayList<>();
-        }
         parent = new int[N + 1];
         for (int i = 0; i < N + 1; i++) {
             parent[i] = i;
-
         }
+
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
+            int command = Integer.parseInt(st.nextToken());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
-            if (a == 0) {
-                union(b, c);
-            } else {
-                if (find(b) == find(c)) {
-                    System.out.println("YES");
-                }else{
-                    System.out.println("NO");
+
+            if (command == 0) { // 합치기
+                union(a, b);
+            } else { // 확인하기
+                if (find(a) == find(b)) {
+                    sb.append(YES).append("\n");
+                } else {
+                    sb.append(NO).append("\n");
                 }
             }
+        }
+        System.out.println(sb);
+    }
 
-
+    public static int find(int a) {
+        if (parent[a] == a) {
+            return a;
+        } else {
+            return parent[a] = find(parent[a]);
         }
     }
 
@@ -48,14 +54,8 @@ class Main {
         a = find(a);
         b = find(b);
         if (a != b) {
-            parent[a] = b;
+            parent[b] = a;
         }
     }
 
-    public static int find(int a) {
-        if (parent[a] == a) {
-            return a;
-        }
-        return parent[a] = find(parent[a]);
-    }
 }
