721edb5d0858c094a075e16ed07bf0df1ddd65ca
권기오 <43674053+kwongio@users.noreply.github.com>
2024-11-27 15:24:19 +0900

    [Gold IV] Title: 행성 연결, Time: 1460 ms, Memory: 147636 KB -BaekjoonHub

---
+++ 백준/Gold/16398. 행성 연결/행성 연결.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int N;
+    static List<int[]> list;
+    static int[] parent;
+    static int[] cost;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        cost = new int[N];
+        list = new ArrayList<>();
+        parent = new int[N + 1];
+        for (int i = 1; i < N + 1; i++) {
+            parent[i] = i;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= N; j++) {
+                int cost = Integer.parseInt(st.nextToken());
+                if(i == j) continue;
+                list.add(new int[]{i, j, cost});
+            }
+        }
+        list.sort(Comparator.comparingInt(value -> value[2]));
+        long ans = 0;
+        for (int[] cur : list) {
+            int a = cur[0];
+            int b = cur[1];
+            int c = cur[2];
+            int max = Math.max(a, b);
+            int min = Math.min(a, b);
+            if (find(min) != find(max)) {
+                union(min, max);
+                ans += c;
+            }
+        }
+        System.out.println(ans);
+    }
+
+    static int find(int a) {
+        if (parent[a] == a) {
+            return a;
+        } else {
+            return parent[a] = find(parent[a]);
+        }
+    }
+
+    static void union(int a, int b) {
+        a = find(a);
+        b = find(b);
+        if (a != b) {
+            parent[b] = a;
+        }
+    }
+}
