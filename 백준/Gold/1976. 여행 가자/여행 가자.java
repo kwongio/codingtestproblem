e9eb4a12025260025628a93198bb9f8eeaab77e5
권기오 <43674053+kwongio@users.noreply.github.com>
2024-11-27 11:15:42 +0900

    [Gold IV] Title: 여행 가자, Time: 176 ms, Memory: 19320 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.java
+++ 백준/Gold/1976. 여행 가자/여행 가자.java
@@ -1,40 +1,40 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
-class Main {
-    static int[] parent;
+public class Main {
     static int N;
     static int M;
+    static List<Integer>[] list;
+    static boolean[] visit;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        M = Integer.parseInt(br.readLine());
-        parent = new int[N + 1];
-        for (int i = 1; i <= N; i++) {
-            parent[i] = i;
+        N = Integer.parseInt(br.readLine()); // 도시의 수
+        M = Integer.parseInt(br.readLine()); // 여행 계획에 속한 도시들의 수
+        list = new ArrayList[N + 1];
+        for (int i = 0; i < N + 1; i++) {
+            list[i] = new ArrayList<>();
         }
-        for (int i = 1; i <= N; i++) {
+
+        for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 1; j <= N; j++) {
+            for (int j = 0; j < N; j++) {
                 if (Integer.parseInt(st.nextToken()) == 1) {
-                    union(i, j);
+                    list[i + 1].add(j + 1);
+                    list[j + 1].add(i + 1);
                 }
             }
         }
-//        for (int i = 0; i < N; i++) {
-//            for (int j = 0; j < N; j++) {
-//                System.out.print(city[i][j]);
-//            }
-//            System.out.println();
-//        }
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int start = find(Integer.parseInt(st.nextToken()));
-        for (int i = 1; i < M; i++) {
-            int now = Integer.parseInt(st.nextToken());
-            if (start != find(now)) {
+        visit = new boolean[N + 1];
+        BFS(Integer.parseInt(st.nextToken()));
+        for (int i = 0; i < M - 1; i++) {
+            if (!visit[Integer.parseInt(st.nextToken())]) {
                 System.out.println("NO");
                 return;
             }
@@ -43,16 +43,18 @@ class Main {
 
     }
 
-    static void union(int a, int b) {
-        a = find(a);
-        b = find(b);
-        if (a != b) {
-            parent[b] = a;
+    private static void BFS(int start) {
+        ArrayDeque<Integer> q = new ArrayDeque<>();
+        visit[start] = true;
+        q.add(start);
+        while (!q.isEmpty()) {
+            Integer now = q.poll();
+            for (Integer next : list[now]) {
+                if (!visit[next]) {
+                    visit[next] = true;
+                    q.add(next);
+                }
+            }
         }
-    }
-
-    static int find(int a) {
-        if (parent[a] == a) return a;
-        else return parent[a] = find(parent[a]);
     }
 }
