706e71bc9010b4663df48c2addc2c765be2d5b23
권기오 <43674053+kwongio@users.noreply.github.com>
2024-11-28 14:45:27 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 608 ms, Memory: 69356 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
@@ -4,20 +4,20 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static int N, E;
+    static int N, M;
     static List<int[]>[] list;
-    static int[] distance;
-    static boolean[] visited;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
-        E = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
         list = new ArrayList[N + 1];
+        for (int i = 0; i < N + 1; i++) {
+            list[i] = new ArrayList<>();
+        }
 
-        for (int i = 1; i <= N; i++) list[i] = new ArrayList<>();
-        for (int i = 0; i < E; i++) {
+        for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
@@ -25,45 +25,41 @@ public class Main {
             list[a].add(new int[]{b, c});
             list[b].add(new int[]{a, c});
         }
+
         st = new StringTokenizer(br.readLine());
-        int v1 = Integer.parseInt(st.nextToken());
-        int v2 = Integer.parseInt(st.nextToken());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
 
-        int result = dijkstra(1, v1);
-        result += dijkstra(v1, v2);
-        result += dijkstra(v2, N);
-
-        int result2 = dijkstra(1, v2);
-        result2 += dijkstra(v2, v1);
-        result2 += dijkstra(v1, N);
-        if(result2 >= 100000001 && result >= 100000001){
+        int result1 = geDis(1, a) + geDis(a, b) + geDis(b, N);
+        int result2 = geDis(1, b) + geDis(b, a) + geDis(a, N);
+        if(result2 >= 100000001 && result1 >= 100000001){
             System.out.println(-1);
         }else{
-             result = Math.min(result2, result);
-            System.out.println(result);
+            result1 = Math.min(result2, result1);
+            System.out.println(result1);
         }
     }
 
-    private static int dijkstra(int s, int e) {
-        visited = new boolean[N + 1];
-        distance = new int[N + 1];
-        for (int i = 1; i < N + 1; i++) {
-            distance[i] = 100000001;
-        }
-        PriorityQueue<int[]> q = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
+    private static int geDis(int s, int e) {
+        PriorityQueue<int[]> q = new PriorityQueue<>((Comparator.comparingInt(o -> o[1])));
         q.add(new int[]{s, 0});
-        distance[s] = 0;
+        int[] dis = new int[N + 1];
+        Arrays.fill(dis, 100000001);
+        boolean[] visit = new boolean[N + 1];
+        dis[s] = 0;
         while (!q.isEmpty()) {
             int[] now = q.poll();
-            if (visited[now[0]]) continue;
-            visited[now[0]] = true;
-            for (int[] next : list[now[0]]) {
-                if (distance[next[0]] > distance[now[0]] + next[1]) {
-                    distance[next[0]] = distance[now[0]] + next[1];
-                    q.add(new int[]{next[0], distance[next[0]]});
+            int cur = now[0];
+            int cnt = now[1];
+            if (visit[cur]) continue;
+            visit[cur] = true;
+            for (int[] next : list[cur]) {
+                if (dis[next[0]] > cnt + next[1]) {
+                    dis[next[0]] = cnt + next[1];
+                    q.add(new int[]{next[0], dis[next[0]]});
                 }
             }
         }
-        return distance[e];
+        return dis[e];
     }
 }
