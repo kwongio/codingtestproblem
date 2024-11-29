c908c1ad7474ec8ea65b3670b142200969efc821
권기오 <43674053+kwongio@users.noreply.github.com>
2024-11-29 11:27:40 +0900

    [Silver III] Title: 터렛, Time: 116 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Silver/1002. 터렛/터렛.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+import static java.lang.System.in;
+
+ class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int T = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < T; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int r1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+            int r2 = Integer.parseInt(st.nextToken());
+            if (x1 == x2 && y1 == y2 && r1 == r2) {
+                System.out.println(-1);
+            } else if (Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2) > Math.pow(r2 + r1, 2)) {
+                System.out.println(0);
+            } else if (Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2) < Math.pow(r2 - r1, 2)) {
+                System.out.println(0);
+            } else if (Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2) == Math.pow(r2 - r1, 2)) {
+                System.out.println(1);
+            } else if (Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2) == Math.pow(r2 + r1, 2)) {
+                System.out.println(1);
+            }else{
+                System.out.println(2);
+            }
+
+
+        }
+    }
+}
