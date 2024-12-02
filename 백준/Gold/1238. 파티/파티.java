c65fba074ec9bd5ac64daa5387c0999116a9f396
권기오 <43674053+kwongio@users.noreply.github.com>
2024-12-02 14:27:28 +0900

    [Gold III] Title: 파티, Time: 176 ms, Memory: 18868 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -7,6 +7,7 @@ public class Main {
     static int N, M;
     static int X;
     static List<int[]>[] list;
+    static List<int[]>[] reverse;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,12 +16,15 @@ public class Main {
         M = Integer.parseInt(st.nextToken());
         X = Integer.parseInt(st.nextToken());
         list = new ArrayList[N + 1];
+        reverse = new ArrayList[N + 1];
         for (int i = 0; i < N + 1; i++) {
             list[i] = new ArrayList<>();
+            reverse[i] = new ArrayList<>();
         }
 
         int[] dis = new int[N + 1];
-        int[] dis1 = new int[N + 1];
+        int[] reverseDis = new int[N + 1];
+        Arrays.fill(reverseDis, Integer.MAX_VALUE);
         Arrays.fill(dis, Integer.MAX_VALUE);
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
@@ -28,20 +32,19 @@ public class Main {
             int b = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
             list[a].add(new int[]{b, c});
+            reverse[b].add(new int[]{a, c});
         }
 
-        extracted(dis, X);
-        int max = Integer.MIN_VALUE;
-        for (int i = 1; i <= N; i++) {
-            if (i == X) continue;
-            Arrays.fill(dis1, Integer.MAX_VALUE);
-            extracted(dis1, i);
-            max = Math.max(max, dis[i] + dis1[X]);
+        geDis(list, dis, X);
+        geDis(reverse, reverseDis, X);
+        int max = 0;
+        for (int i = 1; i < N + 1; i++) {
+            max = Math.max(reverseDis[i] + dis[i], max);
         }
         System.out.println(max);
     }
 
-    private static void extracted(int[] dis, int X) {
+    private static void geDis(List<int[]>[] edge, int[] dis, int X) {
         PriorityQueue<int[]> q = new PriorityQueue<>(((o1, o2) -> o1[1] - o2[1]));
         q.add(new int[]{X, 0});
         boolean[] visit = new boolean[N + 1];
@@ -52,7 +55,7 @@ public class Main {
             int cnt = now[1];
             if (visit[cur]) continue;
             visit[cur] = true;
-            for (int[] next : list[cur]) {
+            for (int[] next : edge[cur]) {
                 if (dis[next[0]] > cnt + next[1]) {
                     dis[next[0]] = cnt + next[1];
                     q.add(new int[]{next[0], dis[next[0]]});
