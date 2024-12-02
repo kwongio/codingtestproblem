2b56e3efef15298b3523b7927ade68f2c8aac845
권기오 <43674053+kwongio@users.noreply.github.com>
2024-12-02 19:00:09 +0900

    [Silver III] Title: 퇴사, Time: 104 ms, Memory: 14336 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -4,18 +4,22 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
     static int max = 0;
     static List<int[]> list;
-    static int N, K;
+    static int N;
+    static int[] dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
+        dp = new int[N];
+        Arrays.fill(dp, -1);
         list = new ArrayList<>();
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
@@ -24,21 +28,23 @@ public class Main {
             list.add(new int[]{a, b});
         }
 
-        DFS(0, 0);
-        System.out.println(max);
+        DFS(0);
+        System.out.println(dp[0]);
     }
 
-    private static void DFS(int day, int money) {
+    private static int DFS(int day) {
         if (day > N) {
-            return;
+            return -999999;
         }
         if (day == N) {
-            max = Math.max(max, money);
-            return;
+            return 0;
+        }
+        if(dp[day] != -1){
+            return dp[day];
         }
 
-        DFS(day + list.get(day)[0], money + list.get(day)[1]);
-        DFS(day + 1, money);
-    }
+        dp[day] = Math.max(DFS(day + list.get(day)[0]) + list.get(day)[1], DFS(day + 1));
 
+        return dp[day];
+    }
 }
