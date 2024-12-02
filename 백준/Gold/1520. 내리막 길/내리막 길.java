4ed63b1db4c6cc99d5cf27c802ff0013641e3896
권기오 <43674053+kwongio@users.noreply.github.com>
2024-12-02 22:34:48 +0900

    [Gold III] Title: 내리막 길, Time: 352 ms, Memory: 37964 KB -BaekjoonHub

---
+++ 백준/Gold/1520. 내리막 길/내리막 길.java
@@ -0,0 +1,58 @@
+import static java.lang.System.in;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static int[][] arr;
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
+    static int[][] dp;
+    static boolean[][] visit;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N][M];
+        dp = new int[N][M];
+        visit = new boolean[N][M];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+            Arrays.fill(dp[i], -1);
+        }
+
+        int dfs = DFS(0, 0);
+        System.out.println(dp[0][0]);
+    }
+
+    private static int DFS(int x, int y) {
+
+        if (x == N - 1 && y == M - 1) {
+            return 1;
+        }
+
+        if (dp[x][y] != -1) {
+            return dp[x][y];
+        }
+
+        int route = 0;
+        for (int i = 0; i < 4; i++) {
+            int nx = dx[i] + x;
+            int ny = dy[i] + y;
+            if (nx >= 0 && nx < N && ny >= 0 && ny < M && arr[x][y] > arr[nx][ny]) {
+                route += DFS(nx, ny);
+            }
+        }
+
+        return dp[x][y] = route;
+    }
+}
