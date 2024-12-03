c91b3e9041b93579e7c8b3b1060271a87696cba8
권기오 <rldh9037@naver.com>
2024-12-03 21:17:59 +0900

    [Silver I] Title: RGB거리, Time: 112 ms, Memory: 18420 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,29 +1,34 @@
+import static java.lang.System.in;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-class Main {
-    static final int RED = 0;
-    static final int BLUE = 1;
-    static final int GREEN = 2;
+public class Main {
     static int N;
+    static int[][] arr;
+    static int RED = 0;
+    static int BLUE = 1;
+    static int GREEN = 2;
+
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        int[][] dp = new int[1000][3];
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        arr = new int[N][N];
         for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            dp[i][RED] = Integer.parseInt(st.nextToken());
-            dp[i][BLUE] = Integer.parseInt(st.nextToken());
-            dp[i][GREEN] = Integer.parseInt(st.nextToken());
+            st = new StringTokenizer(br.readLine());
+            arr[RED][i] = Integer.parseInt(st.nextToken());
+            arr[BLUE][i] = Integer.parseInt(st.nextToken());
+            arr[GREEN][i] = Integer.parseInt(st.nextToken());
         }
         for (int i = 1; i < N; i++) {
-            dp[i][RED] += Math.min(dp[i - 1][BLUE], dp[i - 1][GREEN]);
-            dp[i][BLUE] += Math.min(dp[i - 1][RED], dp[i - 1][GREEN]);
-            dp[i][GREEN] += Math.min(dp[i - 1][RED], dp[i - 1][BLUE]);
+            arr[RED][i] += Math.min(arr[BLUE][i - 1], arr[GREEN][i - 1]);
+            arr[BLUE][i] += Math.min(arr[RED][i - 1], arr[GREEN][i - 1]);
+            arr[GREEN][i] += Math.min(arr[BLUE][i - 1], arr[RED][i - 1]);
         }
-        System.out.println(Math.min(Math.min(dp[N - 1][RED], dp[N - 1][GREEN]), dp[N - 1][BLUE]));
+        System.out.println(Math.min(Math.min(arr[RED][N - 1], arr[BLUE][N - 1]), arr[GREEN][N - 1]));
     }
 }
