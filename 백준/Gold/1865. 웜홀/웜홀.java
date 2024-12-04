ccc81a168f25e03b88d880789911a91c9b4e2ff2
권기오 <rldh9037@naver.com>
2024-12-04 11:25:42 +0900

    [Gold III] Title: 웜홀, Time: 384 ms, Memory: 22864 KB -BaekjoonHub

--- 백준/Gold/1865. 웜홀/웜홀.java
+++ 백준/Gold/1865. 웜홀/웜홀.java
@@ -3,15 +3,14 @@ import static java.lang.System.in;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
-import java.util.PriorityQueue;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
     static int N, M, W, T;
-    static PriorityQueue<int[]>[] list;
-    static boolean[] visit;
+    static List<int[]> list;
     static int[] dis;
 
     public static void main(String[] args) throws IOException {
@@ -21,73 +20,53 @@ public class Main {
         T = Integer.parseInt(st.nextToken());
         for (int i = 0; i < T; i++) {
             st = new StringTokenizer(br.readLine());
-            N = Integer.parseInt(st.nextToken()); // 지점의 수
-            M = Integer.parseInt(st.nextToken()); // 도로 정보
-            W = Integer.parseInt(st.nextToken()); // 웜홀 개수
-            list = new PriorityQueue[N + 1];
-            for (int j = 0; j < N + 1; j++) {
-                list[j] = new PriorityQueue<>(Comparator.comparing(o -> o[1]));
-            }
+            N = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken());
+            W = Integer.parseInt(st.nextToken());
 
+            list = new ArrayList<>();
             for (int j = 0; j < M; j++) {
                 st = new StringTokenizer(br.readLine());
-                int s = Integer.parseInt(st.nextToken()); // 연결 지점
-                int e = Integer.parseInt(st.nextToken()); // 연결 지점
-                int t = Integer.parseInt(st.nextToken()); // 시간
-                list[s].add(new int[]{e, t});
-                list[e].add(new int[]{s, t});
+                int s = Integer.parseInt(st.nextToken());
+                int e = Integer.parseInt(st.nextToken());
+                int t = Integer.parseInt(st.nextToken());
+                list.add(new int[]{s, e, t});
+                list.add(new int[]{e, s, t});
             }
 
             for (int j = 0; j < W; j++) {
                 st = new StringTokenizer(br.readLine());
-                int s = Integer.parseInt(st.nextToken()); // 연결 지점
-                int e = Integer.parseInt(st.nextToken()); // 연결 지점
-                int t = Integer.parseInt(st.nextToken()); // 줄어드는 시산
-                list[s].add(new int[]{e, -t});
+                int s = Integer.parseInt(st.nextToken());
+                int e = Integer.parseInt(st.nextToken());
+                int t = Integer.parseInt(st.nextToken());
+                list.add(new int[]{s, e, -t});
             }
 
-            boolean cycle = false;
-            for (int j = 1; j <= N; j++) {
-                if (getDistance(j)) {
-                    System.out.println("YES");
-                    cycle = true;
-                    break;
-                }
-            }
-            if (!cycle) {
+            if (getDistance()) {
+                System.out.println("YES");
+            } else {
                 System.out.println("NO");
             }
         }
     }
 
-    private static boolean getDistance(int start) {
+    private static boolean getDistance() {
         dis = new int[N + 1];
-        Arrays.fill(dis, Integer.MAX_VALUE);
-        dis[start] = 0;
-        boolean update = false;
+        Arrays.fill(dis, 99999999);
+        dis[1] = 0;
+        for (int i = 1; i <= N; i++) {
+            for (int j = 0; j < list.size(); j++) {
+                int[] next = list.get(j);
+                int cur = next[0];
+                int nx = next[1];
+                int cost = next[2];
 
-        for (int i = 1; i < N; i++) {
-            update = false;
-            for (int j = 1; j <= N; j++) {
-                for (int[] next : list[j]) {
-                    if (dis[j] != Integer.MAX_VALUE && dis[next[0]] > dis[j] + next[1]) {
-                        dis[next[0]] = dis[j] + next[1];
-                        update = true;
-                    }
-                }
-            }
-            if (!update) {
-                break;
-            }
-        }
-        if (update) {
-            for (int i = 1; i <= N; i++) {
-                for (int[] next : list[i]) {
-                    if (dis[i] != Integer.MAX_VALUE && dis[next[0]] > dis[i] + next[1]) {
+                if (dis[nx] > dis[cur] + cost) {
+                    dis[nx] = dis[cur] + cost;
+                    if (i == N) {
                         return true;
                     }
                 }
-
             }
         }
         return false;
