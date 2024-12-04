00eb0f9160125b61f3086c4348b7e15534d22230
권기오 <rldh9037@naver.com>
2024-12-04 15:20:56 +0900

    [Gold IV] Title: 사전 순 최대 공통 부분 수열, Time: 124 ms, Memory: 15880 KB -BaekjoonHub

--- 백준/Gold/30805. 사전 순 최대 공통 부분 수열/사전 순 최대 공통 부분 수열.java
+++ 백준/Gold/30805. 사전 순 최대 공통 부분 수열/사전 순 최대 공통 부분 수열.java
@@ -1,39 +1,38 @@
+import static java.lang.System.in;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    public static List<Integer> sol(List<Integer> arr1, List<Integer> arr2, List<Integer> res) {
-        // 두 배열 중 하나라도 비어 있으면 결과 반환
+    public static List<Integer> DFS(List<Integer> arr1, List<Integer> arr2, List<Integer> res) {
         if (arr1.isEmpty() || arr2.isEmpty()) {
             return res;
         }
 
-        // arr1과 arr2에서 각각 가장 큰 값과 그 인덱스를 찾음
         int tmp1 = max(arr1);
         int tmp2 = max(arr2);
         int idx1 = arr1.indexOf(tmp1);
         int idx2 = arr2.indexOf(tmp2);
 
-        // 두 값이 같으면 결과에 추가하고 그 이후 부분으로 재귀 호출
         if (tmp1 == tmp2) {
             res.add(tmp1);
-            return sol(arr1.subList(idx1 + 1, arr1.size()), arr2.subList(idx2 + 1, arr2.size()), res);
+            return DFS(arr1.subList(idx1 + 1, arr1.size()), arr2.subList(idx2 + 1, arr2.size()), res);
         }
-        // tmp1이 더 크면 arr1에서 제거하고 재귀 호출
         else if (tmp1 > tmp2) {
             arr1.remove(idx1);
-            return sol(arr1, arr2, res);
+            return DFS(arr1, arr2, res);
         }
-        // tmp2가 더 크면 arr2에서 제거하고 재귀 호출
         else {
             arr2.remove(idx2);
-            return sol(arr1, arr2, res);
+            return DFS(arr1, arr2, res);
         }
     }
 
-    // 리스트에서 최대값 찾기
     public static int max(List<Integer> list) {
         int maxVal = list.get(0);
         for (int num : list) {
@@ -44,26 +43,23 @@ public class Main {
         return maxVal;
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
 
-        // 입력 받기
-        int n = sc.nextInt();
+        int n = Integer.parseInt(br.readLine());
         List<Integer> arr1 = new ArrayList<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            arr1.add(sc.nextInt());
+            arr1.add(Integer.parseInt(st.nextToken()));
         }
 
-        int m = sc.nextInt();
+        int m = Integer.parseInt(br.readLine());
+         st = new StringTokenizer(br.readLine());
         List<Integer> arr2 = new ArrayList<>();
         for (int i = 0; i < m; i++) {
-            arr2.add(sc.nextInt());
+            arr2.add(Integer.parseInt(st.nextToken()));
         }
-
-        // 함수 호출
-        List<Integer> ans = sol(arr1, arr2, new ArrayList<>());
-
-        // 결과 출력
+        List<Integer> ans = DFS(arr1, arr2, new ArrayList<>());
         System.out.println(ans.size());
         if (!ans.isEmpty()) {
             for (int num : ans) {
