268476bce944356f75e84c8666c504f5e068fc5c
권기오 <rldh9037@naver.com>
2024-12-04 13:27:02 +0900

    [Silver I] Title: 스티커, Time: 640 ms, Memory: 110136 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.java
+++ 백준/Silver/9465. 스티커/스티커.java
@@ -1,44 +1,42 @@
-import static java.lang.System.in;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N, T;
-    static int[][] arr;
-    static boolean[][] visit;
-    static int[][] dp;
-
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        T = Integer.parseInt(st.nextToken());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
         for (int testCase = 0; testCase < T; testCase++) {
-            int ans = 0;
-            N = Integer.parseInt(br.readLine());
-            st = new StringTokenizer(br.readLine());
-            arr = new int[2][N];
-            dp = new int[2][N + 1];
-            visit = new boolean[2][N];
-            for (int i = 0; i < N; i++) {
-                arr[0][i] = Integer.parseInt(st.nextToken());
+            int N = Integer.parseInt(br.readLine());
+            int[][] arr = new int[2][N];
+            int[][] dp = new int[2][N];
+
+            // 입력 받기
+            for (int i = 0; i < 2; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < N; j++) {
+                    arr[i][j] = Integer.parseInt(st.nextToken());
+                }
             }
 
-            st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < N; i++) {
-                arr[1][i] = Integer.parseInt(st.nextToken());
+            // 초기값 설정
+            dp[0][0] = arr[0][0];
+            dp[1][0] = arr[1][0];
+            if (N > 1) {
+                dp[0][1] = dp[1][0] + arr[0][1];
+                dp[1][1] = dp[0][0] + arr[1][1];
             }
 
-            dp[0][N - 1] = arr[0][N - 1];
-            dp[1][N - 1] = arr[1][N - 1];
-            for (int i = N - 2; i >= 0; i--) {
-                dp[0][i] = Math.max(dp[1][i + 1], dp[1][i + 2]) + arr[0][i];
-                dp[1][i] = Math.max(dp[0][i + 1], dp[0][i + 2]) + arr[1][i];
+            // DP 테이블 채우기
+            for (int i = 2; i < N; i++) {
+                dp[0][i] = Math.max(dp[1][i - 1], dp[1][i - 2]) + arr[0][i];
+                dp[1][i] = Math.max(dp[0][i - 1], dp[0][i - 2]) + arr[1][i];
             }
-            System.out.println(Math.max(dp[0][0], dp[1][0]));
+
+            // 최댓값 출력
+            System.out.println(Math.max(dp[0][N - 1], dp[1][N - 1]));
         }
     }
 }
