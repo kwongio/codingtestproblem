6aa0dcf1b4fd9b77bf4463ae7cbf3a5fd86daaf2
권기오 <rldh9037@naver.com>
2024-12-04 13:30:57 +0900

    [Silver I] Title: 스티커, Time: 604 ms, Memory: 110200 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.java
+++ 백준/Silver/9465. 스티커/스티커.java
@@ -10,33 +10,26 @@ public class Main {
 
         for (int testCase = 0; testCase < T; testCase++) {
             int N = Integer.parseInt(br.readLine());
-            int[][] arr = new int[2][N];
-            int[][] dp = new int[2][N];
+            int[][] arr = new int[2][N + 1];
+            int[][] dp = new int[2][N + 1];
 
             // 입력 받기
             for (int i = 0; i < 2; i++) {
                 StringTokenizer st = new StringTokenizer(br.readLine());
-                for (int j = 0; j < N; j++) {
+                for (int j = 1; j <= N; j++) {
                     arr[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
 
-            // 초기값 설정
-            dp[0][0] = arr[0][0];
-            dp[1][0] = arr[1][0];
-            if (N > 1) {
-                dp[0][1] = dp[1][0] + arr[0][1];
-                dp[1][1] = dp[0][0] + arr[1][1];
-            }
+            dp[0][1] = arr[0][1];
+            dp[1][1] = arr[1][1];
 
-            // DP 테이블 채우기
-            for (int i = 2; i < N; i++) {
+            for (int i = 2; i <= N; i++) {
                 dp[0][i] = Math.max(dp[1][i - 1], dp[1][i - 2]) + arr[0][i];
                 dp[1][i] = Math.max(dp[0][i - 1], dp[0][i - 2]) + arr[1][i];
             }
 
-            // 최댓값 출력
-            System.out.println(Math.max(dp[0][N - 1], dp[1][N - 1]));
+            System.out.println(Math.max(dp[0][N ], dp[1][N]));
         }
     }
 }
