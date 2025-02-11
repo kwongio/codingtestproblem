1521baf914ab1e5662ea2f279ede7d15c855be49
권기오 <rldh9037@naver.com>
2025-02-11 12:58:15 +0900

    [Silver III] Title: 퇴사, Time: 108 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -1,38 +1,37 @@
-import static java.lang.System.in;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int max = 0;
-    static List<int[]> list;
     static int N;
-    static int[] dp;
+    static int[][] map;
+
+    static int max = Integer.MIN_VALUE;
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        dp = new int[N + 1];
-        list = new ArrayList<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        map = new int[N][2];
         for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            list.add(new int[]{a, b});
+            map[i][0] = a;
+            map[i][1] = b;
         }
-        for (int day = 0; day < N ; day++) {
-            if (day + list.get(day)[0] > N) {
-              
-            } else {
-                dp[day + list.get(day)[0]] = Math.max(dp[day + list.get(day)[0]], dp[day] + list.get(day)[1]);
-            }
-            dp[day + 1] = Math.max(dp[day + 1], dp[day]);
+
+        DFS(0, 0);
+        System.out.println(max);
+    }
+
+
+    private static void DFS(int day, int sum) {
+        if (day > N) return;
+        max = Math.max(sum, max);
+        if (day < N) {
+            DFS(day + map[day][0], sum + map[day][1]);
         }
-        System.out.println(dp[N]);
+        DFS(day + 1, sum);
     }
 }
