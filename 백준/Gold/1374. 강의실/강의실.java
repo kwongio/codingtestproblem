75a1ca948b1f0ab05e8bac9e0e542eed5bc6757a
권기오 <rldh9037@naver.com>
2025-02-13 12:26:30 +0900

    [Gold V] Title: 강의실, Time: 656 ms, Memory: 55656 KB -BaekjoonHub

---
+++ 백준/Gold/1374. 강의실/강의실.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    static int N;
+    static List<int[]> list = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            list.add(new int[]{b, c});
+        }
+        list.sort((o1, o2) -> {
+            if (o1[0] == o2[0]) {
+                return o1[1] - o2[1];
+            }
+            return o1[0] - o2[0];
+        });
+
+        PriorityQueue<Integer> q = new PriorityQueue<>();
+
+        for (int[] v : list) {
+            if (q.isEmpty()) {
+                q.add(v[1]);
+            } else {
+                if (v[0] < q.peek()) {
+                    q.add(v[1]);
+                }else{
+                    q.poll();
+                    q.add(v[1]);
+                }
+
+            }
+        }
+        System.out.println(q.size());
+    }
+}
