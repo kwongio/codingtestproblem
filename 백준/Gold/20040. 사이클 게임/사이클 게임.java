df8b6eaed78371748b43f493151f0cd11c4a1e66
권기오 <rldh9037@naver.com>
2025-02-14 15:06:52 +0900

    [Gold IV] Title: 사이클 게임, Time: 536 ms, Memory: 163372 KB -BaekjoonHub

--- 백준/Gold/20040. 사이클 게임/사이클 게임.java
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.java
@@ -1,53 +1,59 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
-class Main {
+public class Main {
 
+    static int N, M;
+    static List<Integer>[] list;
+    static boolean[] visit;
     static int[] parent;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int answer = 1;
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
         parent = new int[N];
+        M = Integer.parseInt(st.nextToken());
         for (int i = 0; i < N; i++) {
             parent[i] = i;
         }
+
+        list = new List[N];
+        for (int i = 0; i < N; i++) {
+            list[i] = new ArrayList<>();
+        }
+
+        visit = new boolean[N];
+
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            if(union(a, b)){
-                answer++;
+            if(find(a) != find(b)){
+                union(a, b);
             }else{
-                System.out.println(answer);
+                System.out.println(i + 1);
                 return;
             }
         }
-
         System.out.println(0);
-
-    }
-
-    static boolean union(int a, int b) {
-        a = find(a);
-        b = find(b);
-        if (a != b) {
-            parent[b] = a;
-            return true;
-        } else {
-            return false;
-        }
-
     }
 
     static int find(int a) {
-        if (parent[a] == a) return a;
-        return parent[a] = find(parent[a]);
+        if (parent[a] == a) {
+            return a;
+        } else {
+            return parent[a] = find(parent[a]);
+        }
     }
-
+    static void union(int a, int b){
+        a = find(a);
+        b = find(b);
+        if(a != b){
+            parent[a] = b;
+        }
+    }
 }
+
