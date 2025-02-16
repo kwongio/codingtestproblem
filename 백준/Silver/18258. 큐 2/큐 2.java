e074303e34ba7d149222a65cac108fcc3290cc14
권기오 <rldh9037@naver.com>
2025-02-16 15:37:15 +0900

    [Silver IV] Title: 큐 2, Time: 1212 ms, Memory: 332920 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.java
+++ 백준/Silver/18258. 큐 2/큐 2.java
@@ -1,83 +1,53 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.StringTokenizer;
-import java.util.Deque;
+import java.io.InputStreamReader;
 import java.util.ArrayDeque;
+import java.util.StringTokenizer;
+
+import static java.lang.System.in;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        ArrayDeque<Integer> q = new ArrayDeque<>();
         StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            String command = st.nextToken();
+            if (command.equals("push")) {
+                q.add(Integer.parseInt(st.nextToken()));
+            } else if (command.equals("pop")) {
+                if (!q.isEmpty()) {
+                    sb.append(q.poll()).append("\n");
+                }else{
+                    sb.append(-1).append("\n");
+                }
+            } else if (command.equals("size")) {
+                sb.append(q.size()).append("\n");
+            } else if (command.equals("empty")) {
+                if (q.isEmpty()) {
+                    sb.append(1).append("\n");
+                }else{
+                    sb.append(0).append("\n");
+                }
 
-        Deque<Integer> q = new ArrayDeque<>();
-
-        int N = Integer.parseInt(br.readLine());
-
-        StringTokenizer command;
-
-        while(N-- > 0) {
-            command = new StringTokenizer(br.readLine(), " ");	// 문자열 분리 
-
-            switch(command.nextToken()) {
-
-                case "push":
-                    // offer는 큐의 맨 뒤에 요소를 추가한다.
-                    q.offer(Integer.parseInt(command.nextToken()));
-                    break;
-
-                case "pop" :
-                    /*
-                     *  poll은 가장 앞에 있는 요소를 삭제하며
-                     *  삭제할 원소가 없을 경우 예외를 던지는 것이 아닌 null을 반환한다.
-                     */
-                    Integer item = q.poll();
-                    if(item == null) {
-                        sb.append(-1).append('\n');
-                    }
-                    else {
-                        sb.append(item).append('\n');
-                    }
-                    break;
-
-                case "size":
-                    sb.append(q.size()).append('\n');
-                    break;
-
-                case "empty":
-                    if(q.isEmpty()) {
-                        sb.append(1).append('\n');
-                    }
-                    else {
-                        sb.append(0).append('\n');
-                    }
-                    break;
-
-                case "front":
-                    // peek()은 큐에 꺼낼 요소가 없을 경우 null을 반환한다.
-                    Integer ite = q.peek();
-                    if(ite == null) {
-                        sb.append(-1).append('\n');
-                    }
-                    else {
-                        sb.append(ite).append('\n');
-                    }
-                    break;
-
-                case "back":
-                    // peekLast()은 큐에 꺼낼 요소가 없을 경우 null을 반환한다.
-                    Integer it = q.peekLast();
-                    if(it == null) {
-                        sb.append(-1).append('\n');
-                    }
-                    else {
-                        sb.append(it).append('\n');
-                    }
-                    break;
+            } else if (command.equals("front")) {
+                if (!q.isEmpty()) {
+                    sb.append(q.peek()).append("\n");
+                }else{
+                    sb.append(-1).append("\n");
+                }
+            } else if (command.equals("back")) {
+                if (!q.isEmpty()) {
+                    sb.append(q.peekLast()).append("\n");
+                }else{
+                    sb.append(-1).append("\n");
+                }
             }
         }
         System.out.println(sb);
+        
     }
 }
