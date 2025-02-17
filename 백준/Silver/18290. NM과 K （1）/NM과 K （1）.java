06cba86d75f322b69eff774471283da3529ccdb6
권기오 <rldh9037@naver.com>
2025-02-17 12:46:09 +0900

    [Silver I] Title: NM과 K (1), Time: 192 ms, Memory: 15992 KB -BaekjoonHub

--- 백준/Silver/18290. NM과 K （1）/NM과 K （1）.java
+++ 백준/Silver/18290. NM과 K （1）/NM과 K （1）.java
@@ -3,6 +3,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+import static java.lang.System.in;
+
 class Main {
     static int N, M, K;
     static int[][] map;
@@ -12,21 +14,19 @@ class Main {
     static int[] dy = {0, 0, 1, -1};
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
         map = new int[N][M];
         visit = new boolean[N][M];
-
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-
         DFS(0, 0, 0, 0);
         System.out.println(max);
     }
@@ -38,11 +38,14 @@ class Main {
         }
 
         for (int i = x; i < N; i++) {
-            for (int j = (i == x ? y : 0); j < M; j++) {  // ✅ 같은 행에서는 y부터, 다음 행은 0부터
-                if (!visit[i][j] && isMove(i, j)) {
-                    visit[i][j] = true;
-                    DFS(i, j, count + 1, total + map[i][j]);
-                    visit[i][j] = false;  // ✅ 백트래킹 (원상 복구)
+            for (int j = (i == x) ? y : 0; j < M; j++) {
+                if (!visit[i][j]) {
+                    if (isMove(i, j)) {
+                        visit[i][j] = true;
+                        DFS(i, j, count + 1, total + map[i][j]);
+                        visit[i][j] = false;
+
+                    }
                 }
             }
         }
