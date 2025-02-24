7fc6d8492862bb0b9cb084035fe23d9b39facec6
권기오 <rldh9037@naver.com>
2025-02-24 19:26:22 +0900

    [level 2] Title: 주식가격, Time: 47.28 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,15 +1,23 @@
 import java.util.*;
+
 class Solution {
     public int[] solution(int[] prices) {
-        int[] answer = new int[prices.length];
-        for(int i = 0 ; i < prices.length;i++){
-            for(int j = i + 1;  j < prices.length; j++){
-                answer[i]++;
-                if(prices[i] > prices[j]){
-                    break;
-                }
+        Stack<Integer> stack = new  Stack<>();
+        int[] ans = new int[prices.length];
+        
+        for(int i =0 ; i < prices.length; i++){
+            while(!stack.isEmpty() && prices[stack.peek()] > prices[i]){
+                ans[stack.peek()] = i - stack.peek();
+                stack.pop();
             }
+            stack.push(i);
         }
-         return answer;
+        
+        while (!stack.isEmpty()) {
+            ans[stack.peek()] = prices.length - stack.peek() - 1;
+            stack.pop();
+        }
+        return ans;
     }
 }
+
