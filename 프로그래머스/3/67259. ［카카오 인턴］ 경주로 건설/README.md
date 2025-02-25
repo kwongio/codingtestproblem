88a2037b9353c9f1090bf352321fbf1607f66f14
권기오 <rldh9037@naver.com>
2025-02-25 17:14:38 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 9.39 ms, Memory: 85.7 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/README.md
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/README.md
@@ -1,10 +1,10 @@
 # [level 3] [카카오 인턴] 경주로 건설 - 67259 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67259) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67259?language=java&gad_source=1&gclid=CjwKCAiAzvC9BhADEiwAEhtlNz-wjgxhBk5DcRLJmym_JXu6XvPmtyu9WKsFIKHM3oVhEllyG30YLBoCvw8QAvD_BwE) 
 
 ### 성능 요약
 
-메모리: 75.7 MB, 시간: 0.26 ms
+메모리: 85.7 MB, 시간: 9.39 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 16일 22:25:53
+2025년 02월 25일 17:14:37
 
 ### 문제 설명
 
--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
@@ -1,66 +1,47 @@
 import java.util.*;
 
 class Solution {
-
     int N;
-    boolean[][][] visited;
-    int[] dx = {-1, 0, 1, 0};
-    int[] dy = {0, 1, 0, -1,};
-
+    int[][][] visit;
+    int min = Integer.MAX_VALUE;
+    int[] dx= { -1,0,1,0};
+    int[] dy = {0,1,0,-1};
     public int solution(int[][] board) {
         N = board.length;
-        visited = new boolean[N][N][4];
-        return bfs(board);
-    }
-
-    public int bfs(int[][] board) {
-        Queue<Node> q = new ArrayDeque<>();
-        q.add(new Node(0, 0, -1, 0));
-        int min_cost = Integer.MAX_VALUE;
-
-        while (!q.isEmpty()) {
-            Node now = q.poll();
-
-            if (now.x == N - 1 && now.y == N - 1) {
-                min_cost = Math.min(min_cost, now.cost);
+        visit = new int[4][N][N];
+        ArrayDeque<int[]> q = new ArrayDeque<>();
+        q.add(new int[]{0,0,0,0});
+        
+        while(!q.isEmpty()){
+            int[] now = q.poll();
+            int x = now[0];
+            int y= now[1];
+            int sum = now[2];
+            int d = now[3];
+            if(min <= sum) continue;
+            if(x == N - 1 && y == N - 1){
+                min = Math.min(min, sum);
                 continue;
             }
+            for(int i =0 ; i < 4; i++){
+                int nx = dx[i] + x;
+                int ny = dy[i] + y;
+                if(nx >=0 && nx < N && ny >=0 && ny < N  && board[nx][ny] ==0 ){
+                    if(i == d || sum == 0){
+                        if(visit[i][nx][ny] > sum + 100 || visit[i][nx][ny] == 0){
+                            q.add(new int[]{nx, ny, sum + 100, i});
+                            visit[i][nx][ny] = sum + 100;
+                        }
 
-            for (int i = 0; i < 4; i++) {
-                int nx = now.x + dx[i];
-                int ny = now.y + this.dy[i];
-
-                if (nx < 0 || nx >= N || ny < 0 || ny >= N || board[nx][ny] == 1) {
-                    continue;
-                }
-
-                int nextCost = now.cost;
-                if (now.dir == -1 || now.dir == i) {
-                    //처음이라 비교대상이 없는 경우엔 무조건 100원만 추가,
-                    //이전과 같은 방향인 경우에도 100원 추가
-                    nextCost += 100;
-                } else {
-                    nextCost += 600;
-                }
-
-                if (!visited[nx][ny][i] || board[nx][ny] >= nextCost) {
-                    q.add(new Node(nx, ny, i, nextCost));
-                    visited[nx][ny][i] =  true;
-                    board[nx][ny] = nextCost;
+                    }else{
+                        if(visit[i][nx][ny] > sum + 600  || visit[i][nx][ny] == 0){
+                            q.add(new int[]{nx, ny, sum + 600, i});
+                            visit[i][nx][ny] = sum + 600;
+                        }
+                    }
                 }
             }
         }
-        return min_cost;
-    }
-
-    public class Node {
-        int x, y, dir, cost;
-
-        public Node(int x, int y, int dir, int cost) {
-            this.x = x;
-            this.y = y;
-            this.dir = dir;
-            this.cost = cost;
-        }
+        return min;
     }
 }
