3f81ff771682c0a3c69b7e03f2c44d7bb5639b8c
권기오 <rldh9037@naver.com>
2025-02-25 16:16:51 +0900

    [level 2] Title: 배달, Time: 4.62 ms, Memory: 93.1 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -1,51 +1,50 @@
 import java.util.*;
-class Solution {
-    static List<int[]>[] list;
 
-    static boolean[] visited;
-    static int[] distance;
+class Solution {
+    int[] dis;
+    List<int[]>[] list;
+    boolean[] visit;
     public int solution(int N, int[][] road, int K) {
         int answer = 0;
-        list = new ArrayList[N + 1];
-        visited = new boolean[N + 1];
-        
-        distance = new int[N + 1];
-        for(int i = 0; i < N + 1; i++){
-            distance[i] = Integer.MAX_VALUE;
-        } 
-        for(int i = 0; i <= N; i++){
+        dis = new int[N + 1];
+        visit = new boolean[N + 1];
+        list = new List[N + 1];
+        for(int i =0 ; i < N + 1; i++){
             list[i] = new ArrayList<>();
         }
-        for(int i =0 ;i < road.length ; i++){   
-            list[road[i][0]].add(new int[]{road[i][1], road[i][2]});
-               list[road[i][1]].add(new int[]{road[i][0], road[i][2]});
+        
+        for(int[] r : road){
+            int a = r[0];
+            int b = r[1];
+            int c = r[2];
+            list[a].add(new int[]{b,c});
+            list[b].add(new int[]{a,c});
         }
-
-        BFS(1);
-        int count = 0;
-        for(int i = 1; i < N + 1; i++){
-            if(distance[i] <= K){
-                count++;
-            }
-        }
-        return count;
-
-  
-    }
-    private static void BFS(int start){
-     PriorityQueue<int[]> q = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
-     q.add(new int[]{start, 0});
-    distance[1] = 0;
+        
+        Arrays.fill(dis, Integer.MAX_VALUE);
+        PriorityQueue<int[]> q = new PriorityQueue<>((o1, o2) -> Integer.compare(o1[1], o2[1]));
+        q.add(new int[]{1, 0});
+        dis[1] = 0;
         while(!q.isEmpty()){
-            int now[] = q.poll();
-            for(int[] n : list[now[0]]){
-                if(distance[n[0]] > n[1] + distance[now[0]]){
-                    distance[n[0]] =n[1] + distance[now[0]];
-                    q.add(new int[]{n[0], distance[n[0]]});
+            int[] now = q.poll();
+            int cur = now[0];
+            int cnt = now[1];
+            if(visit[cur]) continue;
+            visit[cur] = true;
+            for(int[] next : list[cur]){
+                if(dis[next[0]] > next[1] + cnt ){
+                    dis[next[0]] =next[1] + cnt;
+                    q.add(new int[]{next[0], dis[next[0]]});
                 }
             }
-
         }
-       
+        
+        int ans=0;
+        for(int i =1; i < N + 1;i++){
+            if(dis[i] <= K){
+                ans++;
+            }
+        }
+        return ans;
     }
 }
