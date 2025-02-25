35d150dec91d176632c113ed4253905c09a05a12
권기오 <rldh9037@naver.com>
2025-02-25 11:30:19 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 35.30 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -1,34 +1,39 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 class Solution {
-
-    Map<String, String> parent = new HashMap<>();
-    Map<String, Integer> money = new HashMap<>();
-
     public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
-        for (int i = 0; i < enroll.length; i++) {
-            parent.put(enroll[i], referral[i]);
+        int[] answer = new int[enroll.length];
+        Map<String, Integer> map = new HashMap<>();
+        map.put("-", -1);
+        for(int i = 0; i < enroll.length; i++){
+            map.put(enroll[i], i);
+            
         }
-
-        for (int i = 0; i < seller.length; i++) {
-            share(seller[i], amount[i] * 100);
+        
+        for(int i =0 ; i < seller.length; i++){
+            String sname = seller[i];
+            int idx = map.get(sname);
+            int refer = amount[i]*100/10;  //10퍼센트 레퍼럴
+            answer[idx] += (amount[i] * 100 - refer);
+            
+            while(referral[idx] != "-"){ // 그다음 레퍼럴
+                int referIdx = map.get(referral[idx]); // 레퍼럴
+                if(referIdx == -1){
+                    break;
+                }
+                answer[referIdx] += refer; // 레퍼럴에게 주는 돈
+                  if(refer <= 9){
+                    break;
+                }
+                refer = refer/10;
+          
+                // 단, 10% 를 계산할 때에는 원 단위에서 절사하며, 10%를 계산한 금액이 1 원 미만인 경우에는 이득을 분배하지 않고 자신이 모두 가집니다
+                answer[referIdx] -= refer;
+                idx = referIdx;
+                
+            }
         }
-
-        int[] result = new int[enroll.length];
-        for (int i = 0; i < enroll.length; i++) {
-            result[i] = money.getOrDefault(enroll[i], 0);
-        }
-
-        return result;
-    }
-
-    void share(String node, int sales) {
-        int nextSales = sales / 10;
-        money.put(node, money.getOrDefault(node, 0) + sales - nextSales);
-
-        if (nextSales > 0 && parent.containsKey(node)) {
-            share(parent.get(node), nextSales);
-        }
+        
+        return answer;
     }
 }
